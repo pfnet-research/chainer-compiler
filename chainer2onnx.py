631b62437d7a5544c736b2903ea4e284e14c6316
satos---jp <syogo8118@yahoo.co.jp>
2018-08-09 17:13:05 +0900

    initial commit

---
+++ chainer2onnx.py
@@ -0,0 +1,195 @@
+#coding: utf-8
+
+import ast
+import gast
+import sys
+
+from onnx import helper
+
+from onnx import AttributeProto, TensorProto, GraphProto
+from onnx import checker
+
+from chainer import links as L
+
+
+def new_tensor(dims):
+    tn = new_tensor.cnt
+    new_tensor.cnt += 1
+    return helper.make_tensor_value_info('T' + str(tn), TensorProto.FLOAT, dims)
+
+
+new_tensor.cnt = 0
+
+
+def get_dims(tensor):
+    dims = tensor.type.tensor_type.shape.dim
+    return list(map(lambda x: x.dim_value, dims))
+
+
+class Link_Linear:
+    def __init__(sl, ch):
+        sl.name = ch.name
+        sl.n_out = ch.b.shape[0]
+        if not(ch.W.data is None):
+            sl.n_in = ch.W.shape[1]
+        else:
+            sl.n_in = None
+
+        sl.W = helper.make_tensor_value_info(
+            sl.name + '_W', TensorProto.FLOAT, [sl.n_out, ('input_size' if (sl.n_in is None) else sl.n_in)])
+        sl.b = helper.make_tensor_value_info(
+            sl.name + '_b', TensorProto.FLOAT, [sl.n_out])
+
+    def call(sl, args, env):
+        assert(len(args) == 1)
+        v = args[0]
+        res = new_tensor([sl.n_out])
+        env.nodes.append(
+            helper.make_node(
+                "Gemm", inputs=[v.name, sl.W.name, sl.b.name], outputs=[res.name], transA=0, transB=1
+            )
+        )
+        return res
+
+    def init_tensors(sl):
+        return [sl.W, sl.b]
+
+
+class Function_Relu:
+    def call(sl, args, env):
+        assert(len(args) == 1)
+        v = args[0]
+        res = new_tensor(get_dims(v))
+        env.nodes.append(
+            helper.make_node(
+                "Relu", inputs=[v.name], outputs=[res.name]
+            )
+        )
+        return res
+
+
+import os
+
+
+def clip_head(s):
+    s = s.split('\n')
+    # print(s)
+    hs = os.path.commonprefix(list(filter(lambda x: x != '', s)))
+    # print('hs',list(map(ord,hs)))
+    ls = len(hs)
+    s = map(lambda x: x[ls:], s)
+    return '\n'.join(s)
+
+
+import inspect
+
+import code
+
+
+class Env:
+    def __init__(sl, links):
+        sl.links = links
+        sl.vars = {}
+        sl.nodes = []
+
+
+class ValueReturn(Exception):
+    def __init__(sl, value):
+        sl.value = value
+
+
+def eval_ast(nast, env):
+    if isinstance(nast, list):
+        # 逐次実行
+        for s in nast:
+            eval_ast(s, env)
+    elif isinstance(nast, gast.Assign):
+        value = eval_ast(nast.value, env)
+        targs = nast.targets
+        # とりあえずタプル代入はなし
+        assert(len(targs) == 1)
+        env.vars[targs[0].id] = value
+    elif isinstance(nast, gast.Call):
+        fn = eval_ast(nast.func, env)
+        args = list(map(lambda x: eval_ast(x, env), nast.args))
+        return fn.call(args, env)
+    elif isinstance(nast, gast.Attribute):
+        if nast.value.id == env.self_name:  # .selfのとき
+            return env.links[nast.attr]
+        # elif
+        elif nast.value.id == 'F':  # これfunction を F とimport してるかどうかって、関数側からわかんないですよね
+            if nast.attr == 'relu':
+                return Function_Relu()
+            else:
+                raise Exception('unknown function', nast.attr)
+        else:
+            raise Exception('unknown attribute', nast.value.id, '.', nast.attr)
+
+    elif isinstance(nast, gast.Name):
+        return env.vars[nast.id]
+    elif isinstance(nast, gast.Return):
+        raise ValueReturn(eval_ast(nast.value, env))
+    else:
+        raise Exception('unknown ast', nast)
+
+
+def chainer2onnx(model, forward):
+    links = {}
+    for ch in model.children():
+        if isinstance(ch, L.Linear):
+            # print(ch)
+            links[ch.name] = Link_Linear(ch)
+        else:
+            raise Exception('unknown link', ch)
+
+    src = clip_head(inspect.getsource(forward))
+    print(src)
+    tast = gast.ast_to_gast(ast.parse(src))
+
+    tast = tast.body[0]
+    assert(isinstance(tast, gast.gast.FunctionDef))
+
+    env = Env(links)
+    args = list(map(lambda x: x.id, tast.args.args))
+    env.self_name = args[0]
+
+    args = args[1:]
+    # code.InteractiveConsole({'tast':tast}).interact()
+    assert(len(args) == 1)  # とりあえず1入力
+    input_tensors = []
+    for v in args:
+        env.vars[v] = new_tensor(['batch_size', 'input_size'])
+        input_tensors.append(env.vars[v])
+
+    try:
+        eval_ast(tast.body, env)
+        raise Exception('return not found')
+    except ValueReturn as v:
+        output_tensors = [v.value]  # とりあえず1tensor
+
+    for lk in links.values():
+        # print(lk,a)
+        input_tensors += lk.init_tensors()
+
+    # print(env.nodes)
+    # print(input_tensors)
+    # print(output_tensors)
+    # for ch in model.namedparams():
+    #    print(ch)
+
+    graph = helper.make_graph(env.nodes,
+                              'name_is_unknown_now', input_tensors,
+                              output_tensors
+                              )
+
+    # inputのうち、重みであるものにはinitializerをつける
+    # batch_sizeやinput_sizeなどの可変なものはできる限りのそのままで
+
+    checker.check_graph(graph)
+    mo = helper.make_model(graph)
+
+    print(mo)
+    return mo
+
+
+
---
+++ test_mxnet.py
@@ -0,0 +1,157 @@
+#coding: utf-8
+#ほぼ　https://github.com/chainer/onnx-chainer/blob/master/onnx_chainer/testing/test_mxnet.py
+# からもらっってきました
+
+import collections
+import os
+import warnings
+
+import numpy as np
+
+import chainer
+import chainer2onnx
+
+try:
+    import mxnet
+    MXNET_AVAILABLE = True
+except ImportError:
+    warnings.warn(
+        'MXNet is not installed. Please install mxnet to use '
+        'testing utility for compatiblity checking.',
+        ImportWarning)
+    MXNET_AVAILABLE = False
+
+
+from onnx import numpy_helper, helper
+
+def convert_parameter(parameter,name):
+    if isinstance(parameter, chainer.Parameter):
+        array = parameter.array
+    elif isinstance(parameter, chainer.Variable):
+        array = parameter.array
+    elif isinstance(parameter, numpy.ndarray):
+        array = parameter
+    else:
+        raise ValueError(
+            'The type of parameter is unknown. It should be either Parameter '
+            'or Variable or ndarray, but the type was {}.'.format(
+                type(parameter)))
+    if array.shape == ():
+        array = array[None]
+    print('initialize',name,array)
+    return numpy_helper.from_array(array, name)
+
+#入力xから次元を決める
+#モデルにxを流して最初の重みを決める
+def edit_onnx_protobuf(onnxmod,x,chainermod):
+  #chainermod.forward(x)
+  
+  initializers = []
+  for ch in chainermod.children():
+    ln = ch.name
+    for pa in ch.params():
+      wn = pa.name
+      initializers.append(convert_parameter(pa,ln+'_'+wn))
+  
+  dummygraph = helper.make_graph([],"hoge",[],[],initializer = initializers)
+  dummygraph.ClearField("name")
+  #print(dummygraph)
+  onnxmod.graph.MergeFrom(dummygraph)
+
+import onnx_chainer
+from onnx import checker
+
+def check_compatibility(model, x, out_key='prob'):
+    if not MXNET_AVAILABLE:
+        raise ImportError('check_compatibility requires MXNet.')
+    
+     #さらの状態からonnxのmodをつくる
+    onnxmod = chainer2onnx.chainer2onnx(model,model.forward)
+    checker.check_model(onnxmod)
+
+    chainer.config.train = False
+
+    # Forward computation
+    if isinstance(x, (list, tuple)):
+        for i in x:
+            assert isinstance(i, (np.ndarray, chainer.Variable))
+        chainer_out = model(*x)
+    elif isinstance(x, np.ndarray):
+        chainer_out = model(chainer.Variable(x))
+    elif isinstance(x, chainer.Variable):
+        chainer_out = model(x)
+    else:
+        raise ValueError(
+            'The \'x\' argument should be a list or tuple of numpy.ndarray or '
+            'chainer.Variable, or simply numpy.ndarray or chainer.Variable '
+            'itself. But a {} object was given.'.format(type(x)))
+
+    if isinstance(chainer_out, (list, tuple)):
+        chainer_out = [y.array for y in chainer_out]
+    elif isinstance(chainer_out, dict):
+        chainer_out = chainer_out[out_key]
+        if isinstance(chainer_out, chainer.Variable):
+            chainer_out = (chainer_out.array,)
+    elif isinstance(chainer_out, chainer.Variable):
+        chainer_out = (chainer_out.array,)
+    else:
+        raise ValueError('Unknown output type: {}'.format(type(chainer_out)))
+
+
+     #1回の実行をもとにinitialize
+    edit_onnx_protobuf(onnxmod,x,model)
+    
+    fn = 'o.onnx'
+    with open(fn,'wb') as fp:      
+        fp.write(onnxmod.SerializeToString())
+      
+    #onnx_chainer.export(model, x, fn)
+    
+    
+    sym, arg, aux = mxnet.contrib.onnx.import_model(fn)
+
+    data_names = [graph_input for graph_input in sym.list_inputs()
+                  if graph_input not in arg and graph_input not in aux]
+    if len(data_names) > 1:
+        data_shapes = [(n, x_.shape) for n, x_ in zip(data_names, x)]
+    else:
+        data_shapes = [(data_names[0], x.shape)]
+    
+    #print(aux)
+    #print(data_names)
+    print('data shape',data_shapes)
+    import onnx
+    #print(onnx.load('o.onnx'))
+  
+    mod = mxnet.mod.Module(
+        symbol=sym, data_names=data_names, context=mxnet.cpu(),
+        label_names=None)
+    mod.bind(
+        for_training=False, data_shapes=data_shapes,
+        label_shapes=None)
+    mod.set_params(
+        arg_params=arg, aux_params=aux, allow_missing=True,
+        allow_extra=True)
+
+    Batch = collections.namedtuple('Batch', ['data'])
+    if isinstance(x, (list, tuple)):
+        x = [mxnet.nd.array(x_.array) if isinstance(
+            x_, chainer.Variable) else mxnet.nd.array(x_) for x_ in x]
+    elif isinstance(x, chainer.Variable):
+        x = [mxnet.nd.array(x.array)]
+    elif isinstance(x, np.ndarray):
+        x = [mxnet.nd.array(x)]
+
+    mod.forward(Batch(x))
+    mxnet_outs = mod.get_outputs()
+    mxnet_out = [y.asnumpy() for y in mxnet_outs]
+     
+    print(x)
+    print(mxnet_out)
+    print(chainer_out)
+
+    for cy, my in zip(chainer_out, mxnet_out):
+        np.testing.assert_almost_equal(cy, my, decimal=5)
+
+    os.remove(fn)
+
