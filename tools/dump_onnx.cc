efcd2abca57a05b2fc6d0a5461f44eacb20d2dd0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-08-24 15:24:01 +0900

    Add --full flag to dump_onnx

--- tools/dump_onnx.cc
+++ tools/dump_onnx.cc
@@ -9,25 +9,35 @@
 
 #include <common/log.h>
 #include <common/protoutil.h>
+#include <tools/cmdline.h>
 
-int main(int argc, const char** argv) {
-    if (argc <= 1) {
+int main(int argc, char** argv) {
+    cmdline::parser args;
+    args.add("full", '\0', "Dump all tensor values.");
+    args.parse_check(argc, argv);
+
+    if (args.rest().empty()) {
         QFAIL() << "Usage: " << argv[0] << " <onnx>";
     }
 
-    onnx::ModelProto model(LoadLargeProto<onnx::ModelProto>(argv[1]));
-    onnx::GraphProto* graph = model.mutable_graph();
-    for (int i = 0; i < graph->initializer_size(); ++i) {
-        onnx::TensorProto* tensor = graph->mutable_initializer(i);
-#define CLEAR_IF_LARGE(tensor, x) \
-    if (tensor->x().size() >= 20) tensor->clear_##x()
-        CLEAR_IF_LARGE(tensor, float_data);
-        CLEAR_IF_LARGE(tensor, int32_data);
-        CLEAR_IF_LARGE(tensor, string_data);
-        CLEAR_IF_LARGE(tensor, int64_data);
-        CLEAR_IF_LARGE(tensor, raw_data);
-        CLEAR_IF_LARGE(tensor, double_data);
-        CLEAR_IF_LARGE(tensor, uint64_data);
+    for (const std::string& filename : args.rest()) {
+ std::cout << "=== " << filename << " ===\n";
+        onnx::ModelProto model(LoadLargeProto<onnx::ModelProto>(filename));
+        onnx::GraphProto* graph = model.mutable_graph();
+        if (!args.exist("full")) {
+            for (int i = 0; i < graph->initializer_size(); ++i) {
+                onnx::TensorProto* tensor = graph->mutable_initializer(i);
+#define CLEAR_IF_LARGE(tensor, x)                                   \
+                if (tensor->x().size() >= 20) tensor->clear_##x()
+                CLEAR_IF_LARGE(tensor, float_data);
+                CLEAR_IF_LARGE(tensor, int32_data);
+                CLEAR_IF_LARGE(tensor, string_data);
+                CLEAR_IF_LARGE(tensor, int64_data);
+                CLEAR_IF_LARGE(tensor, raw_data);
+                CLEAR_IF_LARGE(tensor, double_data);
+                CLEAR_IF_LARGE(tensor, uint64_data);
+            }
+        }
+        std::cout << model.DebugString();
     }
-    std::cout << model.DebugString();
 }
