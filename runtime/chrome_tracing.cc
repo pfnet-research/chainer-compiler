fabfe1502ff7ff9ab4ae2804b0110dff108a923d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-08-28 16:33:20 +0900

    Add chrome_tracing code

---
+++ runtime/chrome_tracing.cc
@@ -0,0 +1,64 @@
+#include "chrome_tracing.h"
+
+#include <cstdint>
+#include <fstream>
+
+namespace oniku {
+namespace runtime {
+
+ChromeTracingEmitter::ChromeTracingEmitter()
+    : base_time_(std::chrono::system_clock::now()) {
+}
+
+void ChromeTracingEmitter::AddEvent(Event* event) {
+    events_.emplace_back(event);
+}
+
+ChromeTracingEmitter::Event::Event(const std::string& c, const std::string& n)
+    : category(c), name(n), start_time(std::chrono::system_clock::now()) {
+}
+
+void ChromeTracingEmitter::Event::Finish() {
+    end_time = std::chrono::system_clock::now();
+}
+
+ChromeTracingEmitter::ScopedEvent::ScopedEvent(ChromeTracingEmitter* chrome_tracing, const std::string& category, const std::string& name) : event_(nullptr) {
+    if (chrome_tracing) {
+        event_ = new ChromeTracingEmitter::Event(category, name);
+        chrome_tracing->AddEvent(event_);
+    }
+}
+
+ChromeTracingEmitter::ScopedEvent::~ScopedEvent() {
+    if (event_) {
+        event_->Finish();
+    }
+}
+
+void ChromeTracingEmitter::Emit(const std::string& output_filename) const {
+    std::ofstream ofs(output_filename);
+    ofs << "[\n";
+    bool is_first = true;
+    for (const std::unique_ptr<Event>& event : events_) {
+        int64_t ts = std::chrono::duration_cast<std::chrono::microseconds>(event->start_time - base_time_).count();
+        int64_t dur = std::chrono::duration_cast<std::chrono::microseconds>(event->end_time - event->start_time).count();
+
+        if (!is_first) {
+            ofs << ",\n";
+        }
+        is_first = false;
+        ofs << "{";
+        ofs << "\"cat\":\"" << event->category << "\",";
+        ofs << "\"name\":\"" << event->name << "\",";
+        ofs << "\"ts\":" << ts << ",";
+        ofs << "\"dur\":" << dur << ",";
+        ofs << "\"tid\":1,";
+        ofs << "\"pid\":1,";
+        ofs << "\"ph\":\"X\"";
+        ofs << "}";
+    }
+    ofs << "]\n";
+}
+
+}  // namespace runtime
+}  // namespace oniku
---
+++ runtime/chrome_tracing.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace oniku {
+namespace runtime {
+
+class ChromeTracingEmitter {
+public:
+    struct Event {
+        Event(const std::string& c, const std::string& n);
+        void Finish();
+        std::string category;
+        std::string name;
+        std::chrono::system_clock::time_point start_time;
+        std::chrono::system_clock::time_point end_time;
+    };
+
+    class ScopedEvent {
+    public:
+        explicit ScopedEvent(ChromeTracingEmitter* chrome_tracing, const std::string& category, const std::string& name);
+        ~ScopedEvent();
+    private:
+        Event* event_;
+    };
+
+    ChromeTracingEmitter();
+
+    // Takes the ownership of `event`.
+    void AddEvent(Event* event);
+
+    void Emit(const std::string& output_filename) const;
+
+private:
+    std::vector<std::unique_ptr<Event>> events_;
+    std::chrono::system_clock::time_point base_time_;
+};
+
+}  // namespace runtime
+}  // namespace oniku
