6c37f909f32ad1a0cd34a59b20f84b75e9b8ce1c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-08-30 19:04:57 +0900

    Add a script which creates an ONNX model from its ASCII dump

---
+++ scripts/undump_onnx.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+from google.protobuf import text_format
+from onnx import onnx_pb
+
+
+with open(sys.argv[1]) as f:
+    c = f.read()
+m = onnx_pb.ModelProto()
+text_format.Merge(c, m)
+with open(sys.argv[2], 'wb') as f:
+    f.write(m.SerializeToString())
