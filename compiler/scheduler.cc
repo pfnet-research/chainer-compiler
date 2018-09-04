d4f62d45315605166a76d9d9f5971a2c34f25a74
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-04 15:56:54 +0900

    Make it possible to schedule Constant op

--- compiler/scheduler.cc
+++ compiler/scheduler.cc
@@ -18,11 +18,11 @@ void ScheduleComputation(const Graph& graph) {
         q.push(value);
     }
 
-    std::map<const Node*, int> input_counts;
+    std::map<Node*, int> input_counts;
     while (!q.empty()) {
         const Value* value = q.front();
         q.pop();
-        if (const Node* node = value->producer()) {
+        if (Node* node = value->producer()) {
             if (!input_counts.emplace(node, node->inputs().size()).second) continue;
             for (const Value* input : node->inputs()) {
                 q.push(input);
@@ -36,6 +36,21 @@ void ScheduleComputation(const Graph& graph) {
     }
 
     std::vector<Node*> nodes;
+
+    auto schedule_node = [&nodes, &q](Node* node) {
+        nodes.push_back(node);
+        for (const Value* output : node->outputs()) {
+            q.push(output);
+        }
+    };
+
+    // Schedule nodes which are already schedulable (e.g., Constant).
+    for (const auto& p : input_counts) {
+        if (p.second == 0) {
+            schedule_node(p.first);
+        }
+    }
+
     while (!q.empty()) {
         const Value* value = q.front();
         q.pop();
@@ -44,10 +59,7 @@ void ScheduleComputation(const Graph& graph) {
             if (found == input_counts.end()) continue;
             int input_counts = --found->second;
             if (input_counts > 0) continue;
-            nodes.push_back(node);
-            for (const Value* output : node->outputs()) {
-                q.push(output);
-            }
+            schedule_node(node);
         }
     }
 
