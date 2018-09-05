bb1a43a9c13935c1eec67a90fdb82cd5ae6b9766
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-05 13:53:09 +0900

    Introduce XCVMVar to handle sequences properly

---
+++ runtime/xcvm_var.cc
@@ -0,0 +1,60 @@
+#include "xcvm_var.h"
+
+#include <common/log.h>
+#include <common/strutil.h>
+
+namespace oniku {
+namespace runtime {
+
+XCVMVar::XCVMVar(Kind kind)
+    : kind_(kind) {
+    CHECK(kind_ != Kind::kArray);
+}
+
+XCVMVar::XCVMVar(xchainer::Array array)
+    : kind_(Kind::kArray),
+      array_(array) {
+}
+
+xchainer::Array XCVMVar::GetArray() {
+    CHECK(kind_ == Kind::kArray) << static_cast<int>(kind_);
+    return *array_;
+}
+
+std::vector<xchainer::Array>* XCVMVar::GetSequence() {
+    CHECK(kind_ == Kind::kSequence) << static_cast<int>(kind_);
+    return &sequence_;
+}
+
+std::string XCVMVar::ToString() const {
+    switch (kind_) {
+    case Kind::kArray:
+        return array_->shape().ToString();
+    case Kind::kSequence:
+        return StrCat(
+            '[',
+            Join(MapToString(sequence_, [this](const xchainer::Array a) {
+                        return a.shape().ToString();
+                    })),
+            ']');
+    }
+    CHECK(false);
+}
+
+std::string XCVMVar::ToDebugString() const {
+    switch (kind_) {
+    case Kind::kArray:
+        return array_->ToString();
+    case Kind::kSequence:
+        return StrCat(
+            '[',
+            Join(MapToString(sequence_, [this](const xchainer::Array a) {
+                        return a.ToString();
+                    })),
+            ']');
+    }
+    CHECK(false);
+}
+
+}  // namespace runtime
+}  // namespace oniku
---
+++ runtime/xcvm_var.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <nonstd/optional.hpp>
+
+#include <xchainer/array.h>
+
+namespace oniku {
+namespace runtime {
+
+class XCVMVar {
+public:
+    enum class Kind {
+        kArray,
+        kSequence,
+    };
+
+    explicit XCVMVar(Kind kind);
+    explicit XCVMVar(xchainer::Array array);
+
+    xchainer::Array GetArray();
+    std::vector<xchainer::Array>* GetSequence();
+
+    std::string ToString() const;
+    std::string ToDebugString() const;
+
+private:
+    Kind kind_;
+    nonstd::optional<xchainer::Array> array_;
+    std::vector<xchainer::Array> sequence_;
+};
+
+}  // namespace runtime
+}  // namespace oniku
