02bfcd9f73cb6f801fff25b3284717fa611065c1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-06 20:58:35 +0900

    Show the number of differentiable ops

--- scripts/op_coverage.rb
+++ scripts/op_coverage.rb
@@ -10,29 +10,46 @@ end
 
 num_node_tests = Dir.glob('onnx/onnx/backend/test/data/node/*').size
 
-ops = []
+onnx_ops = []
 File.readlines('onnx/docs/Operators.md').each do |line|
   if line =~ /^  \* <a href="#(.*?)"/
-    ops << $1
+    onnx_ops << $1
   end
 end
 
-supported_ops = []
-onikux_ops = []
+grad_ops = []
+File.readlines('compiler/gradient_ops.cc').each do |line|
+  if line =~ /^ *register_grad_fn\(Node::k(.*?), /
+    grad_ops << $1
+  end
+end
+
+ops = []
 File.readlines('compiler/gen_node.py').each do |line|
   if line !~ /^NodeDef\('(.*?)'/
     next
   end
-  op = $1
-  if op =~ /^Onikux/
-    onikux_ops << op
-  else
-    supported_ops << op
-  end
+  ops << $1
 end
 
-puts "Missing ops: #{ops - supported_ops}"
+def categorize(ops)
+  supported_ops = []
+  onikux_ops = []
+  ops.each do |op|
+    if op =~ /^Onikux/
+      onikux_ops << op
+    else
+      supported_ops << op
+    end
+  end
+  [supported_ops, onikux_ops]
+end
 
+supported_ops, onikux_ops = categorize(ops)
+grad_onnx_ops, _ = categorize(grad_ops)
+
+puts "Missing ops: #{ops - supported_ops}"
 puts "Node tests: #{num_passing_node_tests}/#{num_node_tests}"
-puts "Supported ops: #{supported_ops.size}/#{ops.size}"
 puts "Custom ops: #{onikux_ops.size}"
+puts "Differentiable ops: #{grad_onnx_ops.size}"
+puts "Supported ops: #{supported_ops.size}/#{onnx_ops.size}"
