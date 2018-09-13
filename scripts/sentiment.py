062d12bb3ec8007f6fab3e3388c44dbca9280855
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-13 16:57:10 +0900

    Get rid of shape mismatch

--- scripts/sentiment.py
+++ scripts/sentiment.py
@@ -12,6 +12,9 @@ F = chainer.functions
 def _gen_random_sequence(batch_size, sequence_length, num_vocabs):
     lengths = np.random.randint(2, sequence_length, size=batch_size)
     lengths = np.flip(np.sort(lengths), axis=0)
+    # At least a single element should have the maximum sequence
+    # length to avoid a shape mismatch.
+    lengths[0] = sequence_length
     labels = np.random.randint(
         2, num_vocabs, size=(batch_size, sequence_length))
     return labels, lengths
