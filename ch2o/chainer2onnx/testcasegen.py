d73a997d10750bf57e15df524fa57f36890cd4c9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-25 09:58:22 +0900

    A fix for previous commit: dirname, not basename

--- ch2o/chainer2onnx/testcasegen.py
+++ ch2o/chainer2onnx/testcasegen.py
@@ -98,8 +98,8 @@ def generate_testcase(model, xs, out_key='prob'):
     edit_onnx_protobuf(onnxmod, model)
     chainer_out = run_chainer_model(model, xs, out_key)
 
-    if not os.path.exists(os.path.basename(args.output)):
-        os.makedirs(os.path.basename(args.output))
+    if not os.path.exists(os.path.dirname(args.output)):
+        os.makedirs(os.path.dirname(args.output))
     with open(args.output, 'wb') as fp:
         fp.write(onnxmod.SerializeToString())
 
