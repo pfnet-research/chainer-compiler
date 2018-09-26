563d1ee4d02fb2685cdb9353aa868832408a0a2d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-26 18:06:39 +0900

    Get nikucheck.sh run multiple tests

--- scripts/nikucheck.sh
+++ scripts/nikucheck.sh
@@ -6,6 +6,11 @@
 
 set -e
 
-mkdir -p out/tmp
-PYTHONPATH=ch2o python3 "$@" out/tmp
-./tools/run_onnx --test out/tmp
+rm -fr out/ch2o_tmp
+mkdir -p out/ch2o_tmp
+PYTHONPATH=ch2o python3 "$@" out/ch2o_tmp/tmp
+
+for i in out/ch2o_tmp/*; do
+    echo "*** Testing $i ***"
+    ./tools/run_onnx --test $i
+done
