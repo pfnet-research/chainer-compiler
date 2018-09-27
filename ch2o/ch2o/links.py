dea2a32a4c349d7186c0942af44f5826482c001d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-09-27 11:18:24 +0900

    zzz

--- ch2o/ch2o/links.py
+++ ch2o/ch2o/links.py
@@ -294,7 +294,6 @@ class Link_NStepBiLSTM(Callable):
                 ('/%d_bss' % i), TensorProto.FLOAT, ["TODO"])
 
     def call_impl(self, env, hx, cx, xs):
-        assert False  # TODO(hamaji): Re-implement NStepBiLSTM.
         assert hx.value is None  # TODO(hamaji): Not implemented yet.
         assert cx.value is None  # TODO(hamaji): Not implemented yet.
         xs = xs.to_tensor(env)
@@ -349,8 +348,7 @@ class Link_NStepBiLSTM(Callable):
 
             from . chainer2onnx import eval_ast
             import chainer
-            localenv = Env()
-            localenv.module = {}
+            localenv = Env({})
             vs = {
                 'v': v,
                 'F': chainer.functions
