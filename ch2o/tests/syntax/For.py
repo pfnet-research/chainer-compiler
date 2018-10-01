64e2bcb36fb2657019b8d908a020e873b7f707b7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-10-01 13:25:05 +0900

    Remove an unnecessary function

--- ch2o/tests/syntax/For.py
+++ ch2o/tests/syntax/For.py
@@ -4,17 +4,6 @@ import chainer
 import chainer.functions as F
 
 
-def _gen_random_sequence(batch_size, sequence_length, num_vocabs):
-    lengths = np.random.randint(2, sequence_length, size=batch_size)
-    lengths = np.flip(np.sort(lengths), axis=0)
-    # At least a single element should have the maximum sequence
-    # length to avoid a shape mismatch.
-    lengths[0] = sequence_length
-    labels = np.random.randint(
-        2, num_vocabs, size=(batch_size, sequence_length))
-    return labels, lengths
-
-
 class A(chainer.Chain):
     def forward(self, xs, p):
         v = []
