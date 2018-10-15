095ca25948a7835051d668bc91ffb0a088e06cab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-10-15 13:50:13 +0900

    Allow using nikucheck.sh with in-source build

--- scripts/nikucheck.sh
+++ scripts/nikucheck.sh
@@ -13,7 +13,15 @@ rm -fr out/ch2o_tmp
 mkdir -p out/ch2o_tmp
 PYTHONPATH=ch2o python3 "${py}" out/ch2o_tmp/tmp
 
+if [ -e build/CMakeCache.txt ]; then
+    run_onnx=./build/tools/run_onnx
+elif [ -e CMakeCache.txt ]; then
+    run_onnx=./tools/run_onnx
+else
+    run_onnx=./build/tools/run_onnx
+fi
+
 for i in out/ch2o_tmp/*; do
     echo "*** Testing $i ***"
-    ./build/tools/run_onnx --test $i "$@"
+    "${run_onnx}" --test $i "$@"
 done
