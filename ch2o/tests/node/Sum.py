Fix gradient of ReduceSum

ch2o/tests/node/Sum.py
@@ -3,6 +3,7 @@
 import numpy as np
 import chainer
 import chainer.functions as F
+import chainer.links as L
 
 
 class Sum(chainer.Chain):
@@ -25,13 +26,28 @@ class SumAllAxis(chainer.Chain):
         return F.sum(x)
 
 
+class SumBackprop(chainer.Chain):
+    def __init__(self, axis, keepdims):
+        super(SumBackprop, self).__init__()
+        self.axis = axis
+        self.keepdims = keepdims
+        with self.init_scope():
+            self.l = L.Linear(None, 5)
+
+    def forward(self, x):
+        x = self.l(x)
+        x = F.reshape(x, (3, 2, 5))
+        x = F.sum(x, axis=self.axis, keepdims=self.keepdims)
+        return x
+
+
 # ======================================
 
 import ch2o
 
 if __name__ == '__main__':
     np.random.seed(314)
-    a = np.random.rand(3, 5, 4).astype(np.float32)
+    a = np.random.rand(6, 2, 3).astype(np.float32)
 
     ch2o.generate_testcase(Sum(), [a])
 
@@ -40,3 +56,9 @@ if __name__ == '__main__':
     ch2o.generate_testcase(SumTupleAxis(), [a], subname='tuple_axis')
 
     ch2o.generate_testcase(SumAllAxis(), [a], subname='all_axis')
+
+    for axis in [0, 1, 2, (0, 2), (1, 2), None]:
+        for keepdims in [False, True]:
+            name = 'axis%s_kd%s' % (axis, keepdims)
+            ch2o.generate_testcase(lambda: SumBackprop(axis, keepdims), [a],
+                                   subname=name, backprop=True)