657f7ba80414e1e073ff224b7cc25d48a48a3661
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-11-08 19:44:59 +0900

    Simplify FindFusionCandidates a bit

--- compiler/fusion.cc
+++ compiler/fusion.cc
@@ -2,6 +2,7 @@
 
 #include <stack>
 #include <vector>
+#include <set>
 
 #include <compiler/graph.h>
 #include <compiler/node.h>
@@ -31,19 +32,18 @@ void FindFusionCandidates(Graph* graph) {
         q.push(base_node);
         while (!q.empty()) {
             Node* node = q.top();
+            CHECK_EQ(0, node->onikux_fusion_group());
             q.pop();
             if (!cands.emplace(node).second) continue;
 
             for (Value* value : node->inputs()) {
                 Node* next_node = value->producer();
                 if (!next_node) continue;
-                if (next_node->onikux_fusion_group()) continue;
                 if (!fusable_ops.count(next_node->op_type())) continue;
                 q.push(next_node);
             }
             for (Value* value : node->outputs()) {
                 for (Node* next_node : value->users()) {
-                    if (next_node->onikux_fusion_group()) continue;
                     if (!fusable_ops.count(next_node->op_type())) continue;
                     q.push(next_node);
                 }
