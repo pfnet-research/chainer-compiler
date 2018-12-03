ed8e18cd94196af0f58bae6a2a8c382aeb92636c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-03 14:41:01 +0900

    Add a test for the Python API

---
+++ python/oniku_test.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+oniku_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(oniku_root, 'build/python'))
+
+import chainerx
+import numpy as np
+import oniku
+
+def aranges(*shape):
+    r = np.prod(shape)
+    return np.arange(r).reshape(shape).astype(np.float32)
+
+
+def test_inference():
+    model = oniku.load('out/ch2o_node_Linear/model.onnx')
+    params = model.params()
+    input_names = model.input_names()
+    output_names = model.output_names()
+    assert len(input_names) == 1
+    assert len(output_names) == 2
+
+    xcvm = model.compile()
+
+    inputs = dict(params)
+    t1 = chainerx.array(aranges(5, 7))
+    inputs[input_names[0]] = t1
+
+    y1 = chainerx.dot(t1, params['/l1/W'].T) + params['/l1/b']
+    y2 = chainerx.dot(t1, params['/l2/W'].T)
+
+    outputs = xcvm.run(inputs)
+    assert len(outputs) == 2
+
+    chainerx.testing.assert_allclose(y1, outputs[output_names[0]])
+    chainerx.testing.assert_allclose(y2, outputs[output_names[1]])
