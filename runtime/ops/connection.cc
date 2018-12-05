ea36db16d5bb9667b42169bb0268b6a81bdb0cab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-05 17:00:07 +0900

    Move Conv to connection.cc

--- runtime/ops/connection.cc
+++ runtime/ops/connection.cc
@@ -19,5 +19,30 @@ chainerx::Array LinearGradWeightOp::RunImpl(XCVMState* st, const chainerx::Array
     return chainerx::Dot(chainerx::Transpose(gy), xm);
 }
 
+chainerx::Array ConvOp::RunImpl(
+        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const nonstd::optional<chainerx::Array>& b) {
+    return chainerx::Conv(x, w, b, strides, pads);
+}
+
+chainerx::Array ConvTransposeOp::RunImpl(
+        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const nonstd::optional<chainerx::Array>& b) {
+    nonstd::optional<chainerx::StackVector<int64_t, chainerx::kMaxNdim>> out_size = nonstd::nullopt;
+    if (!output_shape.empty()) {
+        out_size = output_shape;
+    }
+    return chainerx::ConvTranspose(x, w, b, strides, pads, out_size);
+}
+
+chainerx::Array ConvTransposeWithDynamicShapeOp::RunImpl(
+        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const chainerx::Array& output_shape) {
+    chainerx::Shape shape = ArrayToShape(output_shape);
+    chainerx::StackVector<int64_t, chainerx::kMaxNdim> out_size(shape.begin() + 2, shape.end());
+    return chainerx::ConvTranspose(x, w, nonstd::nullopt, strides, pads, out_size);
+}
+
+chainerx::Array ConvGradWeightOp::RunImpl(XCVMState* st, const chainerx::Array& w, const chainerx::Array& x, const chainerx::Array& gy) {
+    return x.device().ConvGradWeight(w.dtype(), w.shape(), x, gy, strides, pads, false /* cover_all */);
+}
+
 }  // namespace runtime
 }  // namespace oniku
--- runtime/xcvm_ops.cc
+++ runtime/xcvm_ops.cc
@@ -3,7 +3,6 @@
 #include <chainerx/axes.h>
 #include <chainerx/context.h>
 #include <chainerx/native/native_backend.h>
-#include <chainerx/routines/connection.h>
 #include <chainerx/routines/creation.h>
 #include <chainerx/routines/indexing.h>
 #include <chainerx/routines/linalg.h>
@@ -224,31 +223,6 @@ chainerx::Array ReduceSumToOp::RunImpl(XCVMState* st, const chainerx::Array& dat
 
 chainerx::Array ReduceMeanOp::RunImpl(XCVMState* st, const chainerx::Array& a) {
     return chainerx::Mean(a, GetXchainerAxes(axes), keepdims != 0);
-}
-
-chainerx::Array ConvOp::RunImpl(
-        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const nonstd::optional<chainerx::Array>& b) {
-    return chainerx::Conv(x, w, b, strides, pads);
-}
-
-chainerx::Array ConvTransposeOp::RunImpl(
-        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const nonstd::optional<chainerx::Array>& b) {
-    nonstd::optional<chainerx::StackVector<int64_t, chainerx::kMaxNdim>> out_size = nonstd::nullopt;
-    if (!output_shape.empty()) {
-        out_size = output_shape;
-    }
-    return chainerx::ConvTranspose(x, w, b, strides, pads, out_size);
-}
-
-chainerx::Array ConvTransposeWithDynamicShapeOp::RunImpl(
-        XCVMState* st, const chainerx::Array& x, const chainerx::Array& w, const chainerx::Array& output_shape) {
-    chainerx::Shape shape = ArrayToShape(output_shape);
-    chainerx::StackVector<int64_t, chainerx::kMaxNdim> out_size(shape.begin() + 2, shape.end());
-    return chainerx::ConvTranspose(x, w, nonstd::nullopt, strides, pads, out_size);
-}
-
-chainerx::Array ConvGradWeightOp::RunImpl(XCVMState* st, const chainerx::Array& w, const chainerx::Array& x, const chainerx::Array& gy) {
-    return x.device().ConvGradWeight(w.dtype(), w.shape(), x, gy, strides, pads, false /* cover_all */);
 }
 
 chainerx::Array AbsOp::RunImpl(XCVMState* st, const chainerx::Array& x) {
