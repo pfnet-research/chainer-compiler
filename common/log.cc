a506795ff3b8c8bfe05b9deb9bfaf26e47d5d4a3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-09 17:58:20 +0900

    Wait for gdb on CHECK failure
    if ONIKU_WAIT_ON_CRASH environment variable is set.

--- common/log.cc
+++ common/log.cc
@@ -1,9 +1,23 @@
 #include <common/log.h>
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <cstdlib>
 #include <iostream>
 
 namespace oniku {
+namespace {
+
+void MaybeWait() {
+    if (!std::getenv("ONIKU_WAIT_ON_CRASH")) return;
+    std::cout << "Waiting before crash. You can attach gdb by\n"
+              << "$ gdb -p " << getpid() << std::endl;
+    std::string line;
+    std::getline(std::cin, line);
+}
+
+}  // namespace
 
 FailMessageStream::FailMessageStream(const std::string msg, const char* func, const char* file, int line, bool is_check)
     : msg_(msg), func_(func), file_(file), line_(line), is_check_(is_check) {
@@ -12,9 +26,11 @@ FailMessageStream::FailMessageStream(const std::string msg, const char* func, co
 FailMessageStream::~FailMessageStream() {
     if (is_check_) {
         std::cerr << msg_ << " in " << func_ << " at " << file_ << ":" << line_ << ": " << oss_.str() << std::endl;
+        MaybeWait();
         std::abort();
     } else {
         std::cerr << oss_.str() << std::endl;
+        MaybeWait();
         std::exit(1);
     }
 }
