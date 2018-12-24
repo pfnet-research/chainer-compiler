288188904c5a922f208b5334350700073c2b23f6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-24 18:16:07 +0900

    Add a test for fusion

---
+++ compiler/fusion_test.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include <compiler/fusion.h>
+#include <compiler/graph.h>
+#include <compiler/graph_builder.h>
+
+namespace oniku {
+namespace {
+
+TEST(FusionTest, Basic) {
+    Type type(Dtype::kFloat32, {});
+    Graph graph("test");
+    Value* input = graph.AddInputValue("input", type);
+    Value* output = graph.AddOutputValue("output", type);
+    GraphBuilder gb(&graph, "test", output);
+    Value* tmp = gb.Op(Node::kTanh, {input});
+    gb.Op(Node::kSigmoid, {tmp}, {output});
+
+    FuseOperations(&graph);
+    ASSERT_EQ(1, graph.nodes().size());
+    const Node& node = *graph.nodes()[0];
+    ASSERT_EQ(Node::kOnikuxFusionGroup, node.op_type());
+    ASSERT_TRUE(node.subgraph());
+    EXPECT_EQ(2, node.subgraph()->nodes().size());
+    graph.CheckSanity("fused");
+}
+
+}  // namespace
+}  // namespace oniku
