de698623fbb1c4f62434c2aea3c6aa015db4fdd2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-26 14:11:08 +0900

    Introduce TvmCompiler class

--- compiler/tvm/compiler.cc
+++ compiler/tvm/compiler.cc
@@ -73,70 +73,79 @@ tvm::Tensor GetPlaceholder(const Value* value, const std::string& name) {
                             name);
 }
 
-void BuildTvmProgramImpl(
-    const std::vector<Node*>& nodes, int id, const std::vector<Value*>& inputs, const std::vector<Value*>& outputs, std::string* filename) {
-    CHECK_EQ(1, nodes.size());
-    const Node& node = *nodes[0];
-    CHECK_EQ(node.op_type(), Node::kRelu);
-    const Value* input = node.inputs()[0];
-    // const Value* output = node.outputs()[0];
-
-    tvm::Array<tvm::Expr> tvm_shape{GetShape(input->type())};
-
-    tvm::Tensor in{GetPlaceholder(input, "relu_in")};
-    tvm::Tensor out{topi::relu(in, 0, "relu_out")};
-
-    tvm::Target host{tvm::Target::create("llvm")};
-    tvm::Target target;
-    if (g_use_cuda) {
-        target = tvm::Target{tvm::target::cuda()};
-    } else {
-        target = host;
+class TvmCompiler {
+public:
+    TvmCompiler() {
     }
 
-    tvm::Schedule schedule;
-    if (g_use_cuda) {
-        schedule = topi::cuda::schedule_injective(target, {out});
-    } else {
-        schedule = topi::generic::schedule_injective(target, {out});
+    ~TvmCompiler() {
     }
 
-    tvm::BuildConfig config{tvm::build_config()};
-    tvm::Array<tvm::LoweredFunc> funcs{tvm::lower(schedule, {out, in}, "tvm_op", {}, config)};
+    void Build(const std::vector<Node*>& nodes, int id, const std::vector<Value*>& inputs, const std::vector<Value*>& outputs, std::string* filename) {
+        CHECK_EQ(1, nodes.size());
+        const Node& node = *nodes[0];
 
-    const std::string& dso_name = StrCat("/tmp/liboniku_tvm_op_", id);
+        CHECK_EQ(node.op_type(), Node::kRelu);
+        const Value* input = node.inputs()[0];
+        // const Value* output = node.outputs()[0];
 
-    tvm::runtime::Module module = tvm::build(funcs, target, host, config);
-    CLOG() << module->type_key() << ": " << module->GetSource() << std::endl;
+        tvm::Array<tvm::Expr> tvm_shape{GetShape(input->type())};
 
-    std::vector<std::string> input_files;
+        tvm::Tensor in{GetPlaceholder(input, "relu_in")};
+        tvm::Tensor out{topi::relu(in, 0, "relu_out")};
 
-    const std::string& obj_filename = dso_name + ".o";
-    input_files.push_back(obj_filename);
-    module->SaveToFile(obj_filename, "o");
+        tvm::Target host{tvm::Target::create("llvm")};
+        tvm::Target target;
+        if (g_use_cuda) {
+            target = tvm::Target{tvm::target::cuda()};
+        } else {
+            target = host;
+        }
 
-    if (g_use_cuda) {
-        tvm::runtime::Module& cuda_module = const_cast<tvm::runtime::Module&>(module->imports()[0]);
-        CLOG() << cuda_module->type_key() << ": " << cuda_module->GetSource() << std::endl;
-        const std::string& dev_filename = dso_name + "_dev.c";
-        const std::string& c_code = tvm::codegen::PackImportsToC(module, false /* system_lib */);
-        std::ofstream ofs(dev_filename);
-        ofs << c_code;
-        ofs.close();
-        input_files.push_back(dev_filename);
+        tvm::Schedule schedule;
+        if (g_use_cuda) {
+            schedule = topi::cuda::schedule_injective(target, {out});
+        } else {
+            schedule = topi::generic::schedule_injective(target, {out});
+        }
+
+        tvm::BuildConfig config{tvm::build_config()};
+        tvm::Array<tvm::LoweredFunc> funcs{tvm::lower(schedule, {out, in}, "tvm_op", {}, config)};
+
+        const std::string& dso_name = StrCat("/tmp/liboniku_tvm_op_", id);
+
+        tvm::runtime::Module module = tvm::build(funcs, target, host, config);
+        CLOG() << module->type_key() << ": " << module->GetSource() << std::endl;
+
+        std::vector<std::string> input_files;
+
+        const std::string& obj_filename = dso_name + ".o";
+        input_files.push_back(obj_filename);
+        module->SaveToFile(obj_filename, "o");
+
+        if (g_use_cuda) {
+            tvm::runtime::Module& cuda_module = const_cast<tvm::runtime::Module&>(module->imports()[0]);
+            CLOG() << cuda_module->type_key() << ": " << cuda_module->GetSource() << std::endl;
+            const std::string& dev_filename = dso_name + "_dev.c";
+            const std::string& c_code = tvm::codegen::PackImportsToC(module, false /* system_lib */);
+            std::ofstream ofs(dev_filename);
+            ofs << c_code;
+            ofs.close();
+            input_files.push_back(dev_filename);
+        }
+
+        std::string cmd = StrCat("gcc -shared -fPIC -o ", dso_name, ".so");
+        for (const std::string& input_file : input_files) {
+            cmd += " " + input_file;
+        }
+        CLOG() << "Compile: " << cmd << std::endl;
+        if (system(cmd.c_str()) != 0) {
+            CHECK(false) << strerror(errno) << ": cmd=" << cmd;
+        }
+
+        *filename = dso_name + ".so";
     }
-
-    std::string cmd = StrCat("gcc -shared -fPIC -o ", dso_name, ".so");
-    for (const std::string& input_file : input_files) {
-        cmd += " " + input_file;
-    }
-    CLOG() << "Compile: " << cmd << std::endl;
-    if (system(cmd.c_str()) != 0) {
-        CHECK(false) << strerror(errno) << ": cmd=" << cmd;
-    }
-
-    *filename = dso_name + ".so";
-}
+};
 
 }  // namespace
 
@@ -145,7 +154,8 @@ void BuildTvmProgramImpl(
 void BuildTvmProgram(
     const std::vector<Node*>& nodes, int id, const std::vector<Value*>& inputs, const std::vector<Value*>& outputs, std::string* filename) {
 #if ONIKU_ENABLE_TVM
-    BuildTvmProgramImpl(nodes, id, inputs, outputs, filename);
+    TvmCompiler compiler;
+    compiler.Build(nodes, id, inputs, outputs, filename);
 #else
     CHECK(false) << "Enable -DONIKU_ENABLE_TVM=ON";
 #endif
