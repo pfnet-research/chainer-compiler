8d6dffb718e25d0ddd60a26a6e46f9ef5bb7ba7f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2018-12-29 17:25:15 +0900

    Create conv2d in tophub context

--- python/oniku_tvm.py
+++ python/oniku_tvm.py
@@ -5,12 +5,14 @@ import tvm
 @tvm.api.register_func('oniku.tvm.conv2d')
 def _conv2d(target, inputs, pad_h, pad_w, stride_h, stride_w):
     with target:
-        return topi.nn.conv2d(inputs[0],
-                              inputs[1],
-                              [stride_h, stride_w],
-                              [pad_h, pad_w],
-                              [1, 1],
-                              out_dtype=inputs[0].dtype)
+        tophub_context = tvm.autotvm.tophub.context(target)
+        with tophub_context:
+            return topi.nn.conv2d(inputs[0],
+                                  inputs[1],
+                                  [stride_h, stride_w],
+                                  [pad_h, pad_w],
+                                  [1, 1],
+                                  out_dtype=inputs[0].dtype)
 
 
 @tvm.api.register_func('oniku.tvm.schedule_conv2d')
