951d4b4cc6f607a34717155895c4189c76d91a81
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-01-09 14:12:37 +0900

    Factor out statistics ops from xcvm_ops

--- runtime/xcvm_ops.cc
+++ runtime/xcvm_ops.cc
@@ -58,41 +58,6 @@ chainerx::Array MaxOp::RunImpl(XCVMState* st, const std::vector<chainerx::Array>
     return result;
 }
 
-chainerx::Array ReduceMaxOp::RunImpl(XCVMState* st, const chainerx::Array& a) {
-    return chainerx::AMax(a, GetXchainerAxes(axes), keepdims != 0);
-}
-
-chainerx::Array ReduceSumOp::RunImpl(XCVMState* st, const chainerx::Array& a) {
-    return chainerx::Sum(a, GetXchainerAxes(axes), keepdims != 0);
-}
-
-chainerx::Array ReduceSumSquareOp::RunImpl(XCVMState* st, const chainerx::Array& a) {
-    return chainerx::Sum(a * a, GetXchainerAxes(axes), keepdims != 0);
-}
-
-chainerx::Array ReduceSumToOp::RunImpl(XCVMState* st, const chainerx::Array& data, const chainerx::Array& shape) {
-    const chainerx::Shape& from = data.shape();
-    const chainerx::Shape& to = ArrayToShape(shape);
-    CHECK_GE(from.size(), to.size()) << "Reduce requested but shape actually expands: " << from << " to=" << to;
-
-    chainerx::Axes axes;
-    for (int i = 0; i < from.size(); ++i) {
-        int j = i - from.size() + to.size();
-        if (j < 0) {
-            axes.push_back(i);
-        } else if (from[i] != to[j]) {
-            CHECK_EQ(1, to[j]) << "ReduceSumTo shape mismatches: from=" << from << " to=" << to;
-            axes.push_back(i);
-        }
-    }
-    chainerx::Array result = chainerx::Sum(data, axes, true /* keepdims */);
-    return chainerx::Reshape(result, to);
-}
-
-chainerx::Array ReduceMeanOp::RunImpl(XCVMState* st, const chainerx::Array& a) {
-    return chainerx::Mean(a, GetXchainerAxes(axes), keepdims != 0);
-}
-
 std::tuple<chainerx::Array, chainerx::Array> DropoutOp::RunImpl(XCVMState* st, const chainerx::Array& data) {
     if (st->is_training()) {
         WARN_ONCE("Dropout for training is slow.");
