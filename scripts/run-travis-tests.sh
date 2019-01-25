7d385ec6ad3e89fa85b976e14571b52bf0ede39f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-01-25 18:07:37 +0900

    Use ccache on travis

--- scripts/run-travis-tests.sh
+++ scripts/run-travis-tests.sh
@@ -6,7 +6,11 @@ bash setup.sh
 
 mkdir build
 cd build
-cmake .. -DCHAINER_COMPILER_BUILD_CUDA=OFF -DCHAINER_COMPILER_ENABLE_PYTHON=ON
+cmake .. \
+      -DCHAINER_COMPILER_BUILD_CUDA=OFF \
+      -DCHAINER_COMPILER_ENABLE_PYTHON=ON \
+      -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc \
+      -DCMAKE_C_COMPILER=/usr/lib/ccache/g++
 make -j2
 
 make test
