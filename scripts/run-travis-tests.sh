b52b4773bc1e91800deaf09b88e598129dcb257a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-01-25 16:48:47 +0900

    Add the first .travis.yml, which would not work

---
+++ scripts/run-travis-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euc
+
+mkdir build
+cd build
+cmake .. -DCHAINER_COMPILER_BUILD_CUDA=OFF -DCHAINER_COMPILER_ENABLE_PYTHON=ON
+make -j2
+
+make test
+./scripts/runtests.py
+pytest python
