85bd1c8136328eb16bd182e4398e1d8a0238444e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-01-25 16:57:35 +0900

    Add set up steps to travis

--- scripts/run-travis-tests.sh
+++ scripts/run-travis-tests.sh
@@ -2,6 +2,12 @@
 
 set -euc
 
+sudo apt-get install -y --no-install-recommends \
+     libprotobuf-dev protobuf-compiler libopencv-dev
+
+bash setup.sh
+pip install gast numpy chainer onnx==1.3.0 onnx_chainer
+
 mkdir build
 cd build
 cmake .. -DCHAINER_COMPILER_BUILD_CUDA=OFF -DCHAINER_COMPILER_ENABLE_PYTHON=ON
