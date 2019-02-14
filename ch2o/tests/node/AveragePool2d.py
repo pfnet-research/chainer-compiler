Fix CH2O's average pooling
count_include_pad must be set.

ch2o/tests/node/AveragePool2d.py
@@ -4,15 +4,15 @@ import chainer
 import chainer.functions as F
 
 
-class A(chainer.Chain):
-
-    def __init__(self):
-        super(A, self).__init__()
-
+class AvgPool(chainer.Chain):
     def forward(self, x):
-        # TODO(satos) テストケース増やす
-        # y1 = F.max_pooling_2d(x, (1, 3), stride=(1, 4), pad=(0, 1))
         y1 = F.average_pooling_2d(x, (1, 3), stride=(1, 4))
+        return y1
+
+
+class AvgPoolPad(chainer.Chain):
+    def forward(self, x):
+        y1 = F.average_pooling_2d(x, (3, 4), stride=1, pad=(2, 3))
         return y1
 
 
@@ -23,7 +23,7 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    model = A()
+    x = np.random.rand(2, 3, 19, 13).astype(np.float32)
+    ch2o.generate_testcase(AvgPool, [x])
 
-    x = v = np.random.rand(2, 3, 1, 13).astype(np.float32)
-    ch2o.generate_testcase(model, [x])
+    ch2o.generate_testcase(AvgPoolPad, [x], subname='pad')