e902150332223433cd571b457912752dc8735429
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-26 01:28:31 +0900

    Add the testcase for Unpooling2D

---
+++ ch2o/tests/node/Unpooling2D.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+import chainer
+import chainer.functions as F
+
+
+class Unpooling2D(chainer.Chain):
+    def forward(self, x):
+        y = F.unpooling_2d(x, 2, cover_all=False)
+        return y
+
+
+class Unpooling2D_3x4(chainer.Chain):
+    def forward(self, x):
+        y = F.unpooling_2d(x, (3, 4), cover_all=False)
+        return y
+
+
+# ======================================
+
+import ch2o
+import numpy as np
+
+if __name__ == '__main__':
+    x = np.random.rand(2, 3, 11, 7).astype(np.float32)
+    ch2o.generate_testcase(Unpooling2D, [x])
+    ch2o.generate_testcase(Unpooling2D_3x4, [x], subname='3x4')
