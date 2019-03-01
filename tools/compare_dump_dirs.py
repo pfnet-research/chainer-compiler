68043e0da07a0eb6f27979c7c19b9b9bf6cbf8b4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-03-01 20:04:08 +0900

    Add a tool to compare dump outputs

---
+++ tools/compare_dump_dirs.py
@@ -0,0 +1,39 @@
+# Compare two output dumps created by --dump_output_dir.
+
+import glob
+import os
+import re
+import sys
+
+import numpy as np
+
+
+if len(sys.argv) != 3:
+    sys.stderr.write('Usage: %s <dir1> <dir2>\n' % sys.argv[0])
+    sys.exit(1)
+
+
+def read_dump_dir(d):
+    filenames = sorted(glob.glob(os.path.join(d, '*.npy')))
+    files = []
+    for filename in filenames:
+        name = os.path.basename(filename)
+        matched = re.match(r'\d+_(.*)\.npy$', name)
+        if matched:
+            files.append((matched.group(1), filename))
+    return files
+
+
+files1 = read_dump_dir(sys.argv[1])
+files2 = read_dump_dir(sys.argv[2])
+
+files_map2 = dict(files2)
+
+for name, filename1 in files1:
+    filename2 = files_map2.get(name)
+    if filename2 is None:
+        continue
+    print('Comparing %s' % name)
+    n1 = np.load(filename1)
+    n2 = np.load(filename2)
+    np.testing.assert_allclose(n1, n2)
