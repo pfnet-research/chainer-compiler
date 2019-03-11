e135986bcc5021aa226cc132ad9c3187886f51af
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2019-03-11 16:57:37 +0900

    Implement ostream of Order object

--- compiler/computation_order/core.h
+++ compiler/computation_order/core.h
@@ -2,12 +2,13 @@
 
 #include <string>
 #include <vector>
+#include <ostream>
+
+#include <compiler/graph.h>
+#include <compiler/node.h>
+#include <compiler/value.h>
 
 namespace chainer_compiler {
-
-class Node;
-class Value;
-class Graph;
 
 class Order {
 public:
@@ -26,6 +27,35 @@ public:
 
     Order(Kind kind_, Node* node_, Value* value_)
         : kind(kind_), node(node_), value(value_) {}
+
+    friend std::ostream& operator<<(std::ostream &os, const Order& order);
 };
+
+inline std::ostream& operator<<(std::ostream &os, const Order& order) {
+    os << "Order: ";
+    switch (order.kind) {
+        case Order::kComputeForward: {
+            os << "ComputeForward(node=" << order.node->outputs()[0]->name() << ")";
+            break;
+        }
+        case Order::kComputeBackward: {
+            os << "ComputeBackward(node=" << order.node->outputs()[0]->name() << ")";
+            break;
+        }
+        case Order::kForgetForward: {
+            os << "ForgetForward(value=" << order.value->name() << ")";
+            break;
+        }
+        case Order::kForgetBackward: {
+            os << "ForgetBackward(value=" << order.value->name() << ")";
+            break;
+        }
+        default: {
+            os << "Unknown";
+            break;
+        }
+    }
+    return os;
+}
 
 }  // namespace chainer_compiler
