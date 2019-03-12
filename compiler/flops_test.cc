f1636e37e17c45c0dbb3d894c89219ee871c92c4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-03-12 14:46:58 +0900

    Add test for flops calculation of dilated conv

--- compiler/flops_test.cc
+++ compiler/flops_test.cc
@@ -12,16 +12,23 @@ TEST(FlopsTest, Conv) {
     Graph graph("test");
     Value* x = graph.AddInputValue("x", Type(Dtype::kFloat32, {2, 6, 7, 8}));
     Value* w = graph.AddInputValue("w", Type(Dtype::kFloat32, {4, 6, 3, 2}));
-    Value* y = nullptr;
-    {
-        GraphBuilder gb(&graph, "test", x);
-        y = gb.Op(Node::kConv, {x, w});
-    }
-    int num_unknown_ops = 0;
-    EXPECT_EQ(10080, CalculateFlops(*y->producer(), &num_unknown_ops));
 
-    y->producer()->set_group(2);
-    EXPECT_EQ(5040, CalculateFlops(*y->producer(), &num_unknown_ops));
+    auto make_conv = [&](int group, int dilation) {
+        GraphBuilder gb(&graph, "test", x);
+        Value* y = gb.Op(Node::kConv, {x, w});
+        y->producer()->set_group(group)->set_dilations({dilation, dilation});
+        return y->producer();
+    };
+
+    int num_unknown_ops = 0;
+    EXPECT_EQ(10080, CalculateFlops(*make_conv(1, 1), &num_unknown_ops));
+    EXPECT_EQ(0, num_unknown_ops);
+
+    EXPECT_EQ(5040, CalculateFlops(*make_conv(2, 1), &num_unknown_ops));
+    EXPECT_EQ(0, num_unknown_ops);
+
+    EXPECT_EQ(5184, CalculateFlops(*make_conv(1, 2), &num_unknown_ops));
+    EXPECT_EQ(0, num_unknown_ops);
 }
 
 }  // namespace
