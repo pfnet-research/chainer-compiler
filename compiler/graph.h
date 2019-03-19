53e0a3d4409188f4447eb963edd36b883f369a3d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-03-19 12:00:16 +0900

    Remove Graph::AddConstValue

--- compiler/graph.h
+++ compiler/graph.h
@@ -63,18 +63,6 @@ public:
     Value* AddOutputValue(const std::string& name, const Type& type, int index = -1);
     Value* AddNullValue();
 
-    template <class T>
-    Value* AddConstValue(const std::string& name, const Type& type, const std::vector<T>& data) {
-        Value* value = AddInputValue(name, type);
-        Tensor* t = new Tensor(value->name(), type.dtype(), type.dims(), data);
-        value->ResetInitializer(std::unique_ptr<Tensor>(t));
-        return value;
-    }
-    template <class T>
-    Value* AddConstValue(const std::string& name, const Type& type, const std::initializer_list<T>& data) {
-        return AddConstValue(name, type, std::vector<T>{data});
-    }
-
     Node* AddNode(
             Node::OpType op_type, const std::vector<Value*>& inputs, const std::vector<Value*>& outputs, const std::string& base = "");
 
