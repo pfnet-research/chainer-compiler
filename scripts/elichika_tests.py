f75f9d7d3479b653e08d38c299506d239401483a
durswd <swda.durl@gmail.com>
2019-03-27 15:49:26 +0900

    passed if test

--- scripts/elichika_tests.py
+++ scripts/elichika_tests.py
@@ -32,7 +32,7 @@ TESTS = [
     Generator('syntax', 'Cmp'),
     # Generator('syntax', 'For'),
     # Generator('syntax', 'ForAndIf'),
-    # Generator('syntax', 'If'),
+    Generator('syntax', 'If'),
     # Generator('syntax', 'LinkInFor'),
     Generator('syntax', 'ListComp'),
     Generator('syntax', 'MultiClass'),
