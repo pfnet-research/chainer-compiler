6d38c2c4368b11ccca19959b05b61cfa96f34855
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2019-03-27 18:05:46 +0900

    Revert condition

--- compiler/xcvm/emitter.cc
+++ compiler/xcvm/emitter.cc
@@ -715,7 +715,7 @@ private:
         CHECK_EQ(5UL, node.inputs().size());
         CHECK_EQ(1, node.spatial()) << "`spatial` for BatchNormalization was removed from ONNX";
         size_t num_onnx_outputs = node.outputs().size();
-        if (num_onnx_outputs == 1) {
+        if (num_onnx_outputs == 1 && !node.chainer_in_recomputing()) {
             EMIT(FixedBatchNormalization,
                  GetOutputValue(node, 0),
                  GetValueId(node.input(0)),
