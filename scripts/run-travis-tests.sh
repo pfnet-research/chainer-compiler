9e0dde0e24416450d292d4d0d573f934c11fe752
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-04-04 11:01:24 +0900

    Run MNIST example on travis

--- scripts/run-travis-tests.sh
+++ scripts/run-travis-tests.sh
@@ -25,6 +25,8 @@ cd ..
 ./scripts/runtests.py
 pytest -sv python
 
+python3 examples/mnist/train_mnist.py -d native --compile -e 1 --dump_onnx
+
 # TODO(hamaji): Investigate why running these on Travis takes ~10 minutes.
 # ./build/tools/run_onnx --test out/ch2o_model_Alex_with_loss
 # ./build/tools/run_onnx --test out/ch2o_model_GoogleNet_with_loss
