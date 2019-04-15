c5f5a4afb154b37fc34fb773a186331ec4ec3ef1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-04-15 15:51:27 +0900

    Use logic ops in ChainerX

--- runtime/ops/logic.cc
+++ runtime/ops/logic.cc
@@ -8,16 +8,11 @@ namespace chainer_compiler {
 namespace runtime {
 
 chainerx::Array AndOp::RunImpl(XCVMState* st, const chainerx::Array& a, const chainerx::Array& b) {
-    CHECK_EQ(a.dtype(), chainerx::Dtype::kBool);
-    CHECK_EQ(b.dtype(), chainerx::Dtype::kBool);
-    return a * b;
+    return chainerx::LogicalAnd(a, b);
 }
 
 chainerx::Array OrOp::RunImpl(XCVMState* st, const chainerx::Array& a, const chainerx::Array& b) {
-    CHECK_EQ(a.dtype(), chainerx::Dtype::kBool);
-    CHECK_EQ(b.dtype(), chainerx::Dtype::kBool);
-    // TODO(hamaji): Stop using casts once logical_or is added.
-    return CastTo(CastTo(a, chainerx::Dtype::kInt32) + CastTo(b, chainerx::Dtype::kInt32), chainerx::Dtype::kBool);
+    return chainerx::LogicalOr(a, b);
 }
 
 chainerx::Array XorOp::RunImpl(XCVMState* st, const chainerx::Array& a, const chainerx::Array& b) {
@@ -35,8 +30,7 @@ chainerx::Array GreaterOp::RunImpl(XCVMState* st, const chainerx::Array& a, cons
 }
 
 chainerx::Array GreaterEqualOp::RunImpl(XCVMState* st, const chainerx::Array& a, const chainerx::Array& b) {
-    // TODO(hamaji): This is an incorrect implementation for NaN.
-    return chainerx::LogicalNot(chainerx::Greater(b, a));
+    return chainerx::GreaterEqual(a, b);
 }
 
 chainerx::Array NotOp::RunImpl(XCVMState* st, const chainerx::Array& x) {
