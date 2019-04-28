bb004fc174e837202a6789907d761a555f76700d
okdshin <kokuzen@gmail.com>
2019-04-28 09:06:14 +0000

    make faster and modify `selec` and to use `argsort`

--- runtime/ops/something.cc
+++ runtime/ops/something.cc
@@ -34,14 +34,6 @@ std::vector<Int> argsort(Iter first, Iter last, Comp comp) {
     return indices;
 }
 
-std::array<double, 4> extract_tlbr(chainerx::Array const& a) {
-    const double tly = *(static_cast<float*>(a.raw_data()) + 0);
-    const double tlx = *(static_cast<float*>(a.raw_data()) + 1);
-    const double bry = *(static_cast<float*>(a.raw_data()) + 2);
-    const double brx = *(static_cast<float*>(a.raw_data()) + 3);
-    return std::array<double, 4>{{tly, tlx, bry, brx}};
-}
-
 double calc_area(std::array<double, 4> const& tlbr) {
     return (tlbr[2] - tlbr[0]) * (tlbr[3] - tlbr[1]);
 }
@@ -54,19 +46,16 @@ const T& at(const std::vector<T>& v, size_t i) {
 
 }  // namespace
 
-std::vector<size_t> NonMaximumSuppression(std::vector<chainerx::Array> const& roi_l_list, double nms_threash, size_t nms_limit) {
+std::vector<size_t> NonMaximumSuppression(std::vector<std::array<double, 4>> const& roi_l_list, double nms_threash, size_t nms_limit) {
     std::vector<double> bbox_area_list(roi_l_list.size());
-    std::transform(roi_l_list.begin(), roi_l_list.end(), bbox_area_list.begin(), [](const chainerx::Array& roi) {
-        return calc_area(extract_tlbr(roi));
-    });
-    std::vector<size_t> selec_indices;
+    std::transform(
+            roi_l_list.begin(), roi_l_list.end(), bbox_area_list.begin(), [](const std::array<double, 4>& roi) { return calc_area(roi); });
+    std::vector<size_t> selected_indices;
     for (int64_t i = 0; i < roi_l_list.size(); ++i) {
-        chainerx::Array b = at(roi_l_list, i);
-        const std::array<double, 4> b_tlbr = extract_tlbr(b);
-        const bool is_selected =
-                std::all_of(selec_indices.begin(), selec_indices.end(), [nms_threash, i, b_tlbr, &bbox_area_list, &roi_l_list](size_t s) {
-                    chainerx::Array selected_roi = roi_l_list[s];
-                    const std::array<double, 4> selected_roi_tlbr = extract_tlbr(selected_roi);
+        const std::array<double, 4> b_tlbr = at(roi_l_list, i);
+        const bool is_selected = std::all_of(
+                selected_indices.begin(), selected_indices.end(), [nms_threash, i, b_tlbr, &bbox_area_list, &roi_l_list](size_t s) {
+                    const std::array<double, 4>& selected_roi_tlbr = roi_l_list[s];
                     const std::array<double, 4> tlbr{{std::max(b_tlbr[0], selected_roi_tlbr[0]),
                                                       std::max(b_tlbr[1], selected_roi_tlbr[1]),
                                                       std::min(b_tlbr[2], selected_roi_tlbr[2]),
@@ -76,13 +65,13 @@ std::vector<size_t> NonMaximumSuppression(std::vector<chainerx::Array> const& ro
                     return nms_threash > iou;
                 });
         if (is_selected) {
-            selec_indices.push_back(i);
+            selected_indices.push_back(i);
         }
-        if (selec_indices.size() >= nms_limit) {
+        if (selected_indices.size() >= nms_limit) {
             break;
         }
     }
-    return selec_indices;
+    return selected_indices;
 }
 
 std::vector<chainerx::Array> ChainerCVRPNDecode(
@@ -95,18 +84,19 @@ std::vector<chainerx::Array> ChainerCVRPNDecode(
     const size_t k_nms_limit_post = k_train_nms_limit_post;
     const double in_shape_h = static_cast<double>(AsScalar(in_shape.At({2})));
     const double in_shape_w = static_cast<double>(AsScalar(in_shape.At({3})));
-    std::vector<chainerx::Array> rois_list;
+    std::vector<std::array<double, 4>> rois_list;
     std::vector<chainerx::Array> roi_indices_list;
     const int64_t batch_size = static_cast<int64_t>(chainerx::AsScalar(in_shape.At({0})));
     for (int64_t b = 0; b < batch_size; ++b) {
-        std::vector<chainerx::Array> rois_list_per_batch;
+        std::vector<std::array<double, 4>> rois_list_per_batch;
         std::vector<double> confs_list_per_batch;
         for (size_t l = 0; l < scales.size(); ++l) {
             const float* loc_l = static_cast<float*>(locs[l].raw_data()) + b * locs[l].shape()[1] * locs[l].shape()[2];
             const float* conf_l = static_cast<float*>(confs[l].raw_data()) + b * confs[l].shape()[1];
             const auto& h_l = hs[l];
             const int64_t k_l = locs[l].shape()[1];
-            std::vector<chainerx::Array> roi_l_list;
+            std::vector<std::array<double, 4>> roi_l_list(h_l.shape()[2] * h_l.shape()[3] * k_anchor_ratios.size());
+            size_t ki = 0;
             for (size_t u = 0; u < h_l.shape()[2]; ++u) {
                 for (size_t v = 0; v < h_l.shape()[3]; ++v) {
                     for (size_t iar = 0; iar < k_anchor_ratios.size(); ++iar) {
@@ -130,27 +120,19 @@ std::vector<chainerx::Array> ChainerCVRPNDecode(
                         const double roi_l_w = aw * std::exp(std::min(loc_l_w, k_exp_clip));
 
                         // yxhw -> tlbr (top left, bottom right)
-                        const double roi_l_tly = std::max(roi_l_y - roi_l_h / 2.0, 0.0);
-                        const double roi_l_tlx = std::max(roi_l_x - roi_l_w / 2.0, 0.0);
-                        const double roi_l_bry = std::min(roi_l_y + roi_l_h / 2.0, in_shape_h);
-                        const double roi_l_brx = std::min(roi_l_x + roi_l_w / 2.0, in_shape_w);
-                        auto data = std::shared_ptr<void>(new float[4]{static_cast<float>(roi_l_tly),
-                                                                       static_cast<float>(roi_l_tlx),
-                                                                       static_cast<float>(roi_l_bry),
-                                                                       static_cast<float>(roi_l_brx)});
-                        chainerx::Array roi_l_k = chainerx::FromContiguousHostData({4}, h_l.dtype(), std::move(data));
-                        roi_l_list.push_back(std::move(roi_l_k));
+                        roi_l_list[ki][0] = std::max(roi_l_y - roi_l_h / 2.0, 0.0);  // roi_l_tly
+                        roi_l_list[ki][1] = std::max(roi_l_x - roi_l_w / 2.0, 0.0);  // roi_l_tlx
+                        roi_l_list[ki][2] = std::min(roi_l_y + roi_l_h / 2.0, in_shape_h);  // roi_l_bry
+                        roi_l_list[ki][3] = std::min(roi_l_x + roi_l_w / 2.0, in_shape_w);  // roi_l_brx
+                        ++ki;
                     }  // end for iar
                 }  // end for v
             }  // end for u
+            // printf("%f\n", static_cast<float*>(roi_l_list.at(0).raw_data()));
 
             // reduce size of `conf_l` to `nms_limit_pre`
-            std::vector<size_t> cut_indices(k_l);
-            std::iota(cut_indices.begin(), cut_indices.end(), 0);
-            std::sort(cut_indices.begin(), cut_indices.end(), [&conf_l](size_t i, size_t j) {
-                return *(conf_l + i) > *(conf_l + j);
-            });  // TODO(okada) can we use nth_element?
-            std::vector<chainerx::Array> roi_l_list_cut(std::min(static_cast<size_t>(k_l), k_nms_limit_pre));
+            std::vector<size_t> cut_indices = argsort<size_t>(conf_l, conf_l + k_l, [](float a, float b) { return a > b; });
+            std::vector<std::array<double, 4>> roi_l_list_cut(std::min(static_cast<size_t>(k_l), k_nms_limit_pre));
             std::vector<double> conf_l_list_cut(std::min(static_cast<size_t>(k_l), k_nms_limit_pre));
             std::transform(
                     cut_indices.begin(), cut_indices.begin() + roi_l_list_cut.size(), roi_l_list_cut.begin(), [&roi_l_list](size_t i) {
@@ -164,15 +146,15 @@ std::vector<chainerx::Array> ChainerCVRPNDecode(
             std::vector<size_t> mask_indices(roi_l_list_cut.size());
             std::iota(mask_indices.begin(), mask_indices.end(), 0);
             const auto mask_indices_end_iter = std::remove_if(mask_indices.begin(), mask_indices.end(), [&roi_l_list_cut](size_t i) {
-                const float* p = static_cast<float*>(at(roi_l_list_cut, i).raw_data());
-                const double roi_l_tly = *(p + 0);
-                const double roi_l_tlx = *(p + 1);
-                const double roi_l_bry = *(p + 2);
-                const double roi_l_brx = *(p + 3);
+                const std::array<double, 4>& roi = at(roi_l_list_cut, i);
+                const double roi_l_tly = roi[0];
+                const double roi_l_tlx = roi[1];
+                const double roi_l_bry = roi[2];
+                const double roi_l_brx = roi[3];
                 return (roi_l_bry <= roi_l_tly) || (roi_l_brx <= roi_l_tlx);
             });
             const size_t masked_size = std::distance(mask_indices.begin(), mask_indices_end_iter);
-            std::vector<chainerx::Array> roi_l_list_masked(masked_size);
+            std::vector<std::array<double, 4>> roi_l_list_masked(masked_size);
             std::vector<double> conf_l_list_masked(masked_size);
             std::transform(mask_indices.begin(), mask_indices_end_iter, roi_l_list_masked.begin(), [&roi_l_list_cut](size_t i) {
                 return at(roi_l_list_cut, i);
@@ -183,7 +165,7 @@ std::vector<chainerx::Array> ChainerCVRPNDecode(
 
             // NMS
             std::vector<size_t> indices = NonMaximumSuppression(roi_l_list_masked, k_nms_thresh, k_nms_limit_post);
-            std::vector<chainerx::Array> roi_l_list_suppressed(indices.size());
+            std::vector<std::array<double, 4>> roi_l_list_suppressed(indices.size());
             std::vector<double> conf_l_list_suppressed(indices.size());
             std::transform(indices.begin(), indices.end(), roi_l_list_suppressed.begin(), [&roi_l_list_masked](size_t i) {
                 return at(roi_l_list_masked, i);
@@ -198,22 +180,25 @@ std::vector<chainerx::Array> ChainerCVRPNDecode(
         }  // end for l
 
         // reduce size of `roi_list_per_batch` to `nms_limit_post`
-        auto conf = confs_list_per_batch;
-        std::vector<size_t> nms_post_indices(conf.size());
-        std::iota(nms_post_indices.begin(), nms_post_indices.end(), 0);
-        std::sort(nms_post_indices.begin(), nms_post_indices.end(), [&conf](size_t i, size_t j) {
-            return conf[i] > conf[j];
-        });  // TODO nth_element
-        std::vector<chainerx::Array> rois_list_per_batch_sorted;
+        const auto& conf = confs_list_per_batch;
+        std::vector<size_t> nms_post_indices = argsort<size_t>(conf.begin(), conf.end(), [](double a, double b) { return a > b; });
+        std::vector<std::array<double, 4>> rois_list_per_batch_sorted(std::min(k_nms_limit_post, nms_post_indices.size()));
         std::transform(
                 nms_post_indices.begin(),
-                nms_post_indices.begin() + std::min(k_nms_limit_post, nms_post_indices.size()),
-                std::back_inserter(rois_list_per_batch_sorted),
+                nms_post_indices.begin() + rois_list_per_batch_sorted.size(),
+                rois_list_per_batch_sorted.begin(),
                 [&rois_list_per_batch](size_t i) { return at(rois_list_per_batch, i); });
         rois_list.insert(rois_list.end(), rois_list_per_batch_sorted.begin(), rois_list_per_batch_sorted.end());
         roi_indices_list.push_back(chainerx::Full({static_cast<int64_t>(rois_list.size())}, b));
     }
-    chainerx::Array rois = chainerx::Reshape(chainerx::Concatenate(rois_list), {static_cast<int64_t>(rois_list.size()), 4});
+    chainerx::Array rois = chainerx::Zeros({rois_list.size() * 4}, hs.front().dtype());
+    for (size_t i = 0; i < rois_list.size(); ++i) {
+        *(static_cast<float*>(rois.raw_data()) + i * 4 + 0) = rois_list[i][0];
+        *(static_cast<float*>(rois.raw_data()) + i * 4 + 1) = rois_list[i][1];
+        *(static_cast<float*>(rois.raw_data()) + i * 4 + 2) = rois_list[i][2];
+        *(static_cast<float*>(rois.raw_data()) + i * 4 + 3) = rois_list[i][3];
+    }
+    rois = chainerx::Reshape(rois, {static_cast<int64_t>(rois_list.size()), 4});
     chainerx::Array roi_indices = chainerx::Reshape(chainerx::Concatenate(roi_indices_list), {static_cast<int64_t>(rois_list.size())});
     return std::vector<chainerx::Array>({std::move(rois), std::move(roi_indices)});
 }
