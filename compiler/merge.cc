1fcd8eb8242cae2ac50571a25c8f83cd3b5b31a3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-05-07 15:26:06 +0900

    Add merge.cc

---
+++ compiler/merge.cc
@@ -0,0 +1,70 @@
+#include "compiler/merge.h"
+
+#include <common/iterator.h>
+#include <compiler/graph.h>
+#include <compiler/graph_builder.h>
+#include <compiler/node.h>
+#include <compiler/value.h>
+
+namespace chainer_compiler {
+
+namespace {
+
+bool MaybeMergeSplitConcat(Graph* graph, Node* node) {
+    Node::OpType user_type = node->op_type() == Node::kSplit ? Node::kConcat : Node::kSplit;
+
+    // Check if all outputs are used by a single Concat/Split.
+    Node* user = nullptr;
+    for (Value* output : node->outputs()) {
+        if (output->users().size() != 1) {
+            return false;
+        }
+        if (user && user != output->users()[0]) {
+            return false;
+        }
+        user = output->users()[0];
+    }
+    if (!user) {
+        return false;
+    }
+    if (user->op_type() != user_type) {
+        return false;
+    }
+    if (node->inputs().size() != user->outputs().size()) {
+        return false;
+    }
+    if (node->outputs().size() != user->inputs().size()) {
+        return false;
+    }
+    if (node->axis() != user->axis()) {
+        return false;
+    }
+
+    for (const auto& p : Zip(node->inputs(), user->outputs())) {
+        Value* input;
+        Value* output;
+        std::tie(input, output) = p;
+        GraphBuilder gb(graph, "MergeSplitConcat", output);
+        gb.Op(Node::kIdentity, {input}, output);
+    }
+
+    graph->DetachNode(node);
+    graph->DetachNode(user);
+    return true;
+}
+
+}  // namespace
+
+void MergeOperations(Graph* graph) {
+    bool replaced = true;
+    while (replaced) {
+        replaced = false;
+        for (Node* node : graph->GetLiveNodes()) {
+            if (node->op_type() == Node::kSplit || node->op_type() == Node::kConcat) {
+                replaced |= MaybeMergeSplitConcat(graph, node);
+            }
+        }
+    }
+}
+
+}  // namespace chainer_compiler
