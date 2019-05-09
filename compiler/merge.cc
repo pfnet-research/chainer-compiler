642e855c65d3a91516ccc5c97fe570a24f7eaf7b
take-cheeze <takechi101010@gmail.com>
2019-05-09 06:10:06 +0000

    Comment in begin and end pad check

--- compiler/merge.cc
+++ compiler/merge.cc
@@ -70,14 +70,12 @@ bool MaybeMergePadConv(Graph* graph, Node* pad) {
         return false;
     }
 
-    /*
-    // pads of begin and end should be same
+    // pads of begin and end must be same
     for (auto i = 2; i < pads.size() / 2; ++i) {
         if (pads[i] != pads[pads.size() / 2 + i]) {
             return false;
         }
     }
-    */
 
     // pads must be greater or equal to 0
     if (std::any_of(pads.begin(), pads.end(), [](auto p) { return p < 0; })) {
