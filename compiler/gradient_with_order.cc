e17ad365690e703187cda32912a5f18c870bcf08
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2019-05-16 15:37:55 +0900

    refactor one_phase version of AddGradientNodesForTrainingWithOrders

--- compiler/gradient_with_order.cc
+++ compiler/gradient_with_order.cc
@@ -107,144 +107,6 @@ std::vector<Order> GetComputationOrder(const Graph& graph, const std::string& po
     }
 }
 
-void AddGradientNodesForTrainingWithOrders(Graph* graph, const std::vector<Order>& orders) {
-    // A map from the original value to the staged value, possibly recomputed.
-    std::map<Value*, Value*> staged;
-    for (Value* value : graph->input_values()) {
-        CHECK(staged.emplace(value, value).second);
-    }
-
-    // A map from the original node to the last forward
-    // computation. This scheduler assumes the last forward
-    // computation is the only computation which must care the
-    // backward computation.
-    std::map<Node*, Node*> last_forward_map;
-    std::vector<Node*> scheduled_nodes;
-    auto schedule_recompute = [&staged, &scheduled_nodes, &last_forward_map](Node* node, Node* orig_node) {
-        scheduled_nodes.push_back(node);
-        node->set_chainer_order(scheduled_nodes.size());
-        last_forward_map[orig_node] = node;
-        for (const auto& p : Zip(node->outputs(), orig_node->outputs())) {
-            Value* value = std::get<0>(p);
-            if (!staged.emplace(std::get<1>(p), value).second) {
-                std::cerr << "Forward recompute without forgetting the output: " << orig_node->ToString() << std::endl;
-            }
-        }
-    };
-
-    auto schedule_node = [&schedule_recompute](Node* node) { schedule_recompute(node, node); };
-
-    {
-        ScheduleAddedScope schedule_scope(graph, schedule_node);
-        SetInitialGradients(graph);
-    }
-
-    std::set<Node*> scheduled_forward;
-    for (const Order& order : orders) {
-        switch (order.kind) {
-            case Order::kComputeForward: {
-                Node* node = order.node;
-                CHECK(node);
-                if (scheduled_forward.insert(node).second) {
-                    // The first forward computation. All inputs must
-                    // be staged and not be recomputed.
-                    for (Value* value : node->inputs()) {
-                        auto found = staged.find(value);
-                        CHECK(found != staged.end()) << value->DebugString();
-                        // Not recomputed.
-                        CHECK_EQ(value, found->second);
-                    }
-                    schedule_node(node);
-                } else {
-                    // All inputs must be staged and may be recomputed.
-                    std::vector<Value*> inputs;
-                    for (Value* value : node->inputs()) {
-                        auto found = staged.find(value);
-                        CHECK(found != staged.end()) << "Value " << value->name() << " is not staged.";
-                        inputs.push_back(found->second);
-                    }
-
-                    // Recomputed values need different `Value`
-                    // objects with different names.
-                    std::vector<Value*> outputs;
-                    for (Value* value : node->outputs()) {
-                        Value* new_value = graph->AddValue("Recompute" + value->name());
-                        outputs.push_back(new_value);
-                    }
-
-                    // Copy the original computation node to generate
-                    // node for recomputation.
-                    onnx::NodeProto xnode;
-                    node->ToONNX(&xnode);
-                    Node* new_node = new Node(xnode, inputs, outputs);
-                    graph->AddNodeImpl(std::unique_ptr<Node>(new_node), inputs, outputs);
-                    schedule_recompute(new_node, node);
-                    if (node->op_type() == Node::kBatchNormalization) {
-                        node->set_chainer_in_recomputing(1);
-                    }
-                }
-                break;
-            }
-
-            case Order::kComputeBackward: {
-                Node* orig_node = order.node;
-                auto found = last_forward_map.find(orig_node);
-                CHECK(found != last_forward_map.end());
-                Node* node = found->second;
-
-                // Copy gradients of inputs/outputs from the original
-                // computation node to the last forward computation.
-                // Copying inputs is necessary to accumulate gradients.
-                if (node != orig_node) {
-                    for (const auto& p : Zip(node->inputs(), orig_node->inputs())) {
-                        std::get<0>(p)->set_grad(std::get<1>(p)->grad());
-                    }
-                    for (const auto& p : Zip(node->outputs(), orig_node->outputs())) {
-                        std::get<0>(p)->set_grad(std::get<1>(p)->grad());
-                    }
-                }
-
-                ScheduleAddedScope schedule_scope(graph, schedule_node);
-                if (!AddGradientForNode(graph, graph, node, nullptr)) {
-                    break;
-                    // CHECK(false) << "All ops must be differentiable: " << node->DebugString();
-                }
-
-                // Copy back gradients of inputs from the last forward
-                // computation to the original node.
-                if (node != orig_node) {
-                    for (const auto& p : Zip(orig_node->inputs(), node->inputs())) {
-                        std::get<0>(p)->set_grad(std::get<1>(p)->grad());
-                    }
-                }
-
-                break;
-            }
-
-            case Order::kForgetForward: {
-                auto found = staged.find(order.value);
-                CHECK(found != staged.end()) << order.value->DebugString();
-                staged.erase(found);
-                break;
-            }
-
-            case Order::kForgetBackward:
-                // TODO(hamaji): Do something?
-                break;
-
-            default:
-                CHECK(false) << static_cast<int>(order.kind);
-        }
-    }
-
-    {
-        ScheduleAddedScope schedule_scope(graph, schedule_node);
-        ExposeParamGradsAsOutputs(graph, graph, GetParamValues(graph));
-    }
-
-    graph->ResetGradients();
-}
-
 void AddGradInputs(Graph* fwd_graph, Graph* bwd_graph) {
     for (Value* value : fwd_graph->output_values()) {
         Value* grad = bwd_graph->AddInputValue("grad_in@" + value->name(), value->type());
@@ -271,8 +133,6 @@ void AddRetainedParts(Graph* fwd_graph, Graph* bwd_graph, std::map<Value*, Value
 }
 
 void AddGradientNodesForTrainingWithOrders(Graph* fwd_graph, Graph* bwd_graph, const std::vector<Order>& orders) {
-    // Two phase version
-
     // A map from the original value to the staged value, possibly recomputed.
     std::map<Value*, Value*> staged;
     for (Value* value : fwd_graph->input_values()) {
@@ -299,12 +159,17 @@ void AddGradientNodesForTrainingWithOrders(Graph* fwd_graph, Graph* bwd_graph, c
 
     auto schedule_node = [&schedule_recompute](Node* node) { schedule_recompute(node, node); };
 
+    if (fwd_graph == bwd_graph) {
+        ScheduleAddedScope schedule_scope(fwd_graph, schedule_node);
+        SetInitialGradients(fwd_graph);
+    }
+
     std::set<Node*> scheduled_forward;
     Graph* current_graph = fwd_graph;
 
     for (const Order& order : orders) {
         // Check if we should turn to the backward part
-        if (current_graph == fwd_graph) {
+        if (fwd_graph != bwd_graph && current_graph == fwd_graph) {
             bool output_all_staged = true;
             for (Value* output : fwd_graph->output_values()) {
                 if (!staged.count(output)) output_all_staged = false;
@@ -431,6 +296,10 @@ void AddGradientNodesForTrainingWithOrders(Graph* fwd_graph, Graph* bwd_graph, c
 
     fwd_graph->ResetGradients();
     bwd_graph->ResetGradients();
+}
+
+void AddGradientNodesForTrainingWithOrders(Graph* graph, const std::vector<Order>& orders) {
+    AddGradientNodesForTrainingWithOrders(graph, graph, orders);
 }
 
 }  // namespace chainer_compiler
