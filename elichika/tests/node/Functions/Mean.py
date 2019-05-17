7afa1b762834de8e1dbfd519a1cb24b7004d4569
durswd <swda.durl@gmail.com>
2019-05-17 18:07:20 +0900

    fixed tests

--- elichika/tests/node/Functions/Mean.py
+++ elichika/tests/node/Functions/Mean.py
@@ -27,7 +27,7 @@ class MeanAllAxis(chainer.Chain):
 
 # ======================================
 
-if __name__ == '__main__':
+def main():
     np.random.seed(314)
     a = np.random.rand(3, 5, 4).astype(np.float32)
 
@@ -38,3 +38,6 @@ if __name__ == '__main__':
     testtools.generate_testcase(MeanTupleAxis(), [a], subname='tuple_axis')
 
     testtools.generate_testcase(MeanAllAxis(), [a], subname='all_axis')
+
+if __name__ == '__main__':
+    main()
