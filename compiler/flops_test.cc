4de370fe27084a25cfd8a1b2aa84fcb6d8ee25b8
take-cheeze <takechi101010@gmail.com>
2019-05-21 05:34:30 +0000

    Add test of clip/sigmoid/leaky relu

--- compiler/flops_test.cc
+++ compiler/flops_test.cc
@@ -31,5 +31,27 @@ TEST(FlopsTest, Conv) {
     EXPECT_EQ(0, num_unknown_ops);
 }
 
+TEST(FlopsTest, IntegralMultipleOfOutputSize) {
+    Graph graph("test");
+    Value* in = graph.AddInputValue("input", Type(Dtype::kFloat32, {2, 6}));
+
+    auto run_test = [&](Node::OpType op, int mul) {
+        Node* n;
+        {
+            GraphBuilder gb(&graph, "test", in);
+            Value* out = gb.Op(op, {in});
+            n = out->producer();
+        }
+
+        int num_unknown_ops = 0;
+        EXPECT_EQ(2 * 6 * mul, CalculateFlops(*n, &num_unknown_ops));
+        EXPECT_EQ(0, num_unknown_ops);
+    };
+
+    run_test(Node::kClip, 2);
+    run_test(Node::kSigmoid, 4);
+    run_test(Node::kLeakyRelu, 2);
+}
+
 }  // namespace
 }  // namespace chainer_compiler
