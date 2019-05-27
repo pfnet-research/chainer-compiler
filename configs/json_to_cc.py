34c95feb69cc4e1171df8527ca7b72519a01719f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-05-27 21:52:24 +0900

    Validate the JSON at compile time

--- configs/json_to_cc.py
+++ configs/json_to_cc.py
@@ -11,6 +11,8 @@ def main():
     c = open(sys.argv[1]).read()
     # Strip comments.
     c = re.subn(r'//.*\n', '', c)[0]
+    # Check if it is a valid JSON.
+    json.loads(c)
     c = json.dumps(c)
     print('namespace chainer_compiler {')
     print('namespace builtin_configs {')
