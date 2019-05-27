1a275487e8cbd9cb06c3c27cf882d075797d8504
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-05-27 14:03:51 +0900

    Implement inheritence in JSON repository

--- configs/backend_config_test.cc
+++ configs/backend_config_test.cc
@@ -9,6 +9,16 @@ TEST(BackendConfigTest, ChxVM) {
     std::unique_ptr<BackendConfig> config = BackendConfig::FromName("chxvm");
     EXPECT_EQ(1, config->GetSimplifiers().count("ReplaceLess"));
     EXPECT_EQ(0, config->GetSimplifiers().count("NOT FOUND"));
+    EXPECT_EQ(1, config->GetSimplifiers().count("ReplaceChainerSelectItem"));
+    EXPECT_EQ(0, config->GetSimplifiers().count("ReplaceChainerLinear"));
+}
+
+TEST(BackendConfigTest, ChxVMTest) {
+    std::unique_ptr<BackendConfig> config = BackendConfig::FromName("chxvm_test");
+    EXPECT_EQ(1, config->GetSimplifiers().count("ReplaceLess"));
+    EXPECT_EQ(0, config->GetSimplifiers().count("NOT FOUND"));
+    EXPECT_EQ(0, config->GetSimplifiers().count("ReplaceChainerSelectItem"));
+    EXPECT_EQ(1, config->GetSimplifiers().count("ReplaceChainerLinear"));
 }
 
 }  // namespace
--- configs/json_repository.cc
+++ configs/json_repository.cc
@@ -24,6 +24,17 @@ json LoadJSONFromName(const std::string& name) {
 
 json LoadJSONFromString(const std::string& json_str) {
     json j = json::parse(json_str);
+
+    auto found = j.find("base");
+    if (found != j.end()) {
+        CHECK(found->is_string()) << "`base` must be a string: " << json_str;
+        json base = LoadJSONFromName(found->get<std::string>());
+        // See https://tools.ietf.org/html/rfc7396
+        base.merge_patch(j);
+        j = base;
+        j.erase("base");
+    }
+
     return j;
 }
 
