9f9cda562c011698bf9e94e85116e756a698fb43
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-06-01 02:42:53 +0900

    Adjust flags for run_onnx

--- scripts/run-travis-tests.sh
+++ scripts/run-travis-tests.sh
@@ -45,9 +45,11 @@ run train_mnist python3 examples/mnist/train_mnist.py \
 run tools_dump ./build/tools/dump out/ch2o_model_MLP_with_loss
 
 run run_onnx_verbose \
-    ./build/tools/run_onnx --test out/ch2o_model_MLP_with_loss --verbose
+    ./build/tools/run_onnx --test out/ch2o_model_MLP_with_loss \
+    --verbose --compiler_log
 run run_onnx_trace \
-    ./build/tools/run_onnx --test out/ch2o_model_EspNet_E2E --trace
+    ./build/tools/run_onnx --test out/ch2o_model_EspNet_E2E --trace \
+    2>&1 | head -100
 
 run run_onnx_alex \
     ./build/tools/run_onnx --test out/ch2o_model_Alex_with_loss
