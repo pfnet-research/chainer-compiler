3d75d6e542a7bbb353bd37332024d064f1635340
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2019-06-07 18:59:46 +0900

    set PYTHONPATH in CI script

--- .chainerci/run-chainerci-tests.sh
+++ .chainerci/run-chainerci-tests.sh
@@ -41,8 +41,8 @@ make large_tests
 make test
 cd ..
 
-python3 scripts/runtests.py -g --fuse
-python3 scripts/runtests.py --ngraph
+PYTHONPATH=. python3 scripts/runtests.py -g --fuse
+PYTHONPATH=. python3 scripts/runtests.py --ngraph
 python3 -m pytest -sv python
 
 EOM
