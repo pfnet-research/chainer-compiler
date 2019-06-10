6321927ed8cf453509b64bc41452d55cbb6a6257
Daisuke Tanaka <duaipp@gmail.com>
2019-06-10 11:22:01 +0900

    add ch2o tests

--- .chainerci/run-chainerci-pip-tests.sh
+++ .chainerci/run-chainerci-pip-tests.sh
@@ -11,14 +11,23 @@ CUDNN_ROOT_DIR=/usr/local/cuda/lib MAKEFLAGS=-j8 python3 -m pip install -e . -vv
 
 for gen in `ls scripts | grep -E "^gen_.*\.py$"`
 do
-  python3 scripts/$gen
+  python3 scripts/${gen}
 done
 for dir in model node node/ndarray node/Functions node/Links syntax
 do
-  PYTHONPATH=. python3 scripts/elichika_tests.py --generate $dir
+  PYTHONPATH=. python3 scripts/elichika_tests.py --generate ${dir}
 done
 
-PYTHONPATH=. python3 scripts/runtests.py "(^(test_))|(^(backprop_))|(^(extra_))|(^(elichika_)).*$"  -g --fuse
+for dir in model node syntax
+do
+  for op in `ls testcases/ch2o_tests/$dir | sed 's/\.[^\.]*$//'`
+  do
+    PYTHONPATH=. python3 testcases/ch2o_tests/${dir}/${op}.py out/ch2o_${dir}_${op} --quiet && true
+  done
+done
+
+# scripts/runtests.py needs run_onnx binary, skip the test
+
 PYTHONPATH=. python3 -m pytest -sv tests
 
 EOM
