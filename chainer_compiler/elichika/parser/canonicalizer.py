158451a89020f559b050aca561c7b5c4dfbb6316
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-06-11 18:32:43 +0530

    feat: Added astmonkey visualization.

--- chainer_compiler/elichika/parser/canonicalizer.py
+++ chainer_compiler/elichika/parser/canonicalizer.py
@@ -69,16 +69,30 @@ if __name__ == '__main__':
     import ast
     import gast
     import sys
+    from copy import deepcopy
+    try:
+        from astmonkey import transformers, visitors
+        IMPORT_ASTMONKEY = True
+    except ImportError:
+        IMPORT_ASTMONKEY = False
 
-    def dump_ast(mod):
-        mod = gast.ast_to_gast(mod)
-        for b in mod.body:
-            print(ast.dump(b))
+    def dump_ast(mod, name):
+        print(gast.dump(mod))
+        if IMPORT_ASTMONKEY:
+            mod = deepcopy(mod)
+            mod = transformers.ParentChildNodeTransformer().visit(deepcopy(mod))
+            visitor = visitors.GraphNodeVisitor()
+            visitor.visit(mod)
+            visitor.graph.write_png(name + '.png')
+            print("\033[1;32;40mAST visualization saved as \033[94m%s.png\033[0m" % name)
+        else:
+            print("\033[93mInstall astmonkey for visualization.\033[0m")
 
     code = open(sys.argv[1]).read()
-    orig_ast = ast.parse(code)
+    orig_ast = gast.ast_to_gast(ast.parse(code))
     print('=== Original AST ===')
-    dump_ast(orig_ast)
+    dump_ast(orig_ast, 'original')
+
     print('=== Canonicalized AST ===')
     canon_ast = Canonicalizer().visit(orig_ast)
-    dump_ast(canon_ast)
+    dump_ast(canon_ast, 'canonicalized')
