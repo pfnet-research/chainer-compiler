416d21d4efe01241ce5a979227459c844d2d2420
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-06-12 21:28:53 +0530

    test: Added more test cases for Break and Continue.

--- testcases/elichika_tests/canonicalizer/Break_test.py
+++ testcases/elichika_tests/canonicalizer/Break_test.py
@@ -30,9 +30,49 @@ class Break(unittest.TestCase):
         """)
         orig_ast = gast.ast_to_gast(ast.parse(orig_code))
         target_ast = gast.ast_to_gast(ast.parse(target_code))
+        converted_ast = self.canonicalizer.visit(orig_ast)
 
         assert_semantically_equals(orig_code, target_code, ['x', 'i'])
-        assert compare_ast(self.canonicalizer.visit(orig_ast), target_ast)
+        assert compare_ast(converted_ast, target_ast)
+        assert compare_ast(target_ast, converted_ast)
+
+    def test_break_nested(self):
+        orig_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            if i == 5:
+                break
+            for j in range(10):
+                if j == 5:
+                    break
+                x += i * j
+        """)
+        target_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            breaked_0 = False
+            if i == 5:
+                breaked_0 = True
+            if not breaked_0:
+                for j in range(10):
+                    breaked_1 = False
+                    if j == 5:
+                        breaked_1 = True
+                    if not breaked_1:
+                        x += i * j
+                    if breaked_1:
+                        break
+            if breaked_0:
+                break
+        """)
+        orig_ast = gast.ast_to_gast(ast.parse(orig_code))
+        target_ast = gast.ast_to_gast(ast.parse(target_code))
+        converted_ast = self.canonicalizer.visit(orig_ast)
+
+        assert_semantically_equals(orig_code, target_code, ['x', 'i', 'j'])
+        assert compare_ast(converted_ast, target_ast)
+        assert compare_ast(target_ast, converted_ast)
+
 
 
 # ======================================
--- testcases/elichika_tests/canonicalizer/Continue_test.py
+++ testcases/elichika_tests/canonicalizer/Continue_test.py
@@ -28,9 +28,87 @@ class Continue(unittest.TestCase):
         """)
         orig_ast = gast.ast_to_gast(ast.parse(orig_code))
         target_ast = gast.ast_to_gast(ast.parse(target_code))
+        converted_ast = self.canonicalizer.visit(orig_ast)
 
         assert_semantically_equals(orig_code, target_code, ['x', 'i'])
-        assert compare_ast(self.canonicalizer.visit(orig_ast), target_ast)
+        assert compare_ast(converted_ast, target_ast)
+        assert compare_ast(target_ast, converted_ast)
+
+    def test_continue_nested(self):
+        orig_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            if i == 5:
+                continue
+            for j in range(10):
+                if j == 5:
+                    continue
+                x += i * j
+        """)
+        target_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            continued_0 = False
+            if i == 5:
+                continued_0 = True
+            if not continued_0:
+                for j in range(10):
+                    continued_1 = False
+                    if j == 5:
+                        continued_1 = True
+                    if not continued_1:
+                        x += i * j
+        """)
+        orig_ast = gast.ast_to_gast(ast.parse(orig_code))
+        target_ast = gast.ast_to_gast(ast.parse(target_code))
+        converted_ast = self.canonicalizer.visit(orig_ast)
+
+        assert_semantically_equals(orig_code, target_code, ['x', 'i', 'j'])
+        assert compare_ast(converted_ast, target_ast)
+        assert compare_ast(target_ast, converted_ast)
+
+    def test_continue_break_nested(self):
+        orig_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            if i == 5:
+                continue
+            if i == 6:
+                break
+            for j in range(10):
+                if j == 5:
+                    break
+                x += i * j
+        """)
+        target_code = utils.clip_head("""
+        x = 0
+        for i in range(10):
+            breaked_1 = False
+            continued_0 = False
+            if i == 5:
+                continued_0 = True
+            if not continued_0:
+                if i == 6:
+                    breaked_1 = True
+            if not continued_0 and not breaked_1:
+                for j in range(10):
+                    breaked_2 = False
+                    if j == 5:
+                        breaked_2 = True
+                    if not breaked_2:
+                        x += i * j
+                    if breaked_2:
+                        break
+            if breaked_1:
+                break
+        """)
+        orig_ast = gast.ast_to_gast(ast.parse(orig_code))
+        target_ast = gast.ast_to_gast(ast.parse(target_code))
+        converted_ast = self.canonicalizer.visit(orig_ast)
+
+        assert_semantically_equals(orig_code, target_code, ['x', 'i', 'j'])
+        assert compare_ast(converted_ast, target_ast)
+        assert compare_ast(target_ast, converted_ast)
 
 
 # ======================================
