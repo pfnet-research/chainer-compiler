c0bc27abe1cf74d11cdb28f4878fb059d91260fc
take-cheeze <takechi101010@gmail.com>
2019-06-14 16:57:29 +0900

    Show pip package list

--- scripts/run-travis-tests.sh
+++ scripts/run-travis-tests.sh
@@ -20,6 +20,8 @@ run() {
 run pip_chainer sudo pip3 install third_party/chainer
 run pip_onnx_chainer sudo pip3 install third_party/onnx-chainer
 
+run pip_list pip3 list
+
 run setup_sh bash setup.sh
 
 mkdir build
