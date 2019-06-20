34fc33853c0b2a25c08dd81fdac2ef72d5335b4a
take-cheeze <takechi101010@gmail.com>
2019-06-20 17:05:19 +0900

    Make 3D test pass

--- runtime/ops/math.cc
+++ runtime/ops/math.cc
@@ -14,6 +14,8 @@
 #include <runtime/chainerx_util.h>
 #include <runtime/gen_chxvm_ops.h>
 
+#include <numeric>
+
 namespace chainer_compiler {
 namespace runtime {
 
@@ -147,8 +149,23 @@ chainerx::Array ClipOp::RunImpl(ChxVMState* st, const chainerx::Array& x) {
 }
 
 chainerx::Array MatMulOp::RunImpl(ChxVMState* st, const chainerx::Array& a, const chainerx::Array& b) {
-    // TODO(hamaji): Handle non 2D arrays.
-    return chainerx::Dot(a, b);
+    if (a.shape().size() <= 2) {
+        return chainerx::Dot(a, b);
+    }
+
+    // TODO(take-cheeze): Better broadcasting compatibility with numpy
+    if (chainerx::Shape(a.shape().begin(), a.shape().end() - 2) != chainerx::Shape(b.shape().begin(), b.shape().end() - 2)) {
+        return chainerx::Dot(a, b);
+    }
+
+    const int64_t stack_len = std::accumulate(a.shape().begin(), a.shape().end() - 2, 1, std::multiplies<int64_t>());
+    std::vector<chainerx::Array> stack(stack_len);
+    chainerx::Array reshaped_a = a.Reshape({stack_len, *(a.shape().end() - 2), *(a.shape().end() - 1)});
+    chainerx::Array reshaped_b = b.Reshape({stack_len, *(b.shape().end() - 2), *(b.shape().end() - 1)});
+    for (int i = 0; i < stack_len; ++i) {
+        stack[i] = Dot(reshaped_a.At({i}), reshaped_b.At({i}));
+    }
+    return chainerx::Stack(stack);
 }
 
 chainerx::Array GemmOp::RunImpl(ChxVMState* st, const chainerx::Array& a, const chainerx::Array& b, const chainerx::Array& c) {
