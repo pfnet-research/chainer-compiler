9074ad97075a1e5b824577137d9d313d6c7b66ec
Daisuke Tanaka <duaipp@gmail.com>
2019-06-27 18:22:17 +0900

    remove gpu, runtime as option

--- .chainerci/run_onnx.sh
+++ .chainerci/run_onnx.sh
@@ -13,5 +13,5 @@ python3 utils/run_onnx_${CHINAER_COMPILER_RUNTIME}.py data/resnet50 -I 10
 
 EOM
 
-docker run --runtime=nvidia --memory-swap=-1 --rm -v=$(pwd):/chainer-compiler --workdir=/chainer-compiler \
+docker run ${CHINAER_COMPILER_DOCKER_RUNTIME_ARG} --memory-swap=-1 --rm -v=$(pwd):/chainer-compiler --workdir=/chainer-compiler \
     disktnk/chainer-compiler:ci-base-22b692b /bin/bash /chainer-compiler/runtest.sh ${CHINAER_COMPILER_RUNTIME}
