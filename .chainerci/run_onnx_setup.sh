4e62d0a6e264458f5bdb2839735616f5da3ac13c
Daisuke Tanaka <duaipp@gmail.com>
2019-06-27 16:09:38 +0900

    fix tvm setup

--- .chainerci/run_onnx_setup.sh
+++ .chainerci/run_onnx_setup.sh
@@ -7,8 +7,6 @@ cat /proc/meminfo
 
 mkdir -p data
 cd data
-wget -q https://www.cntk.ai/OnnxModels/mnist/opset_7/mnist.tar.gz && \
-    tar -xzf mnist.tar.gz
 wget -q https://s3.amazonaws.com/download.onnx/models/opset_8/resnet50.tar.gz && \
     tar -xzf resnet50.tar.gz
 cd ..
--- .chainerci/run_onnx_setup_tvm.sh
+++ .chainerci/run_onnx_setup_tvm.sh
@@ -5,10 +5,8 @@ set -eux
 . .chainerci/run_onnx_setup.sh
 nvidia-smi
 
-
-ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
-LD_LIBRARY_PATH=/root/tvm_dist/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/root/tvm_dist/lib:$LD_LIBRARY_PATH
 
 CHAINER_VERSION=$(python3 -c "import imp;print(imp.load_source('_version','third_party/chainer/chainer/_version.py').__version__)")
-python3 -m pip install cupy-cuda100==$CHAINER_VERSION
+python3 -m pip install cupy-cuda101==$CHAINER_VERSION onnx==1.5.0
 python3 -m pip list -v
