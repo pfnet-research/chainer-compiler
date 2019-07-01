6f219afdeafe4471a98f6f12adc28d677e26355f
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-07-02 04:26:26 +0530

    test: Buggy behaviour in UserDefinedFunc.

--- testcases/elichika_tests/syntax/UserDefinedFunc.py
+++ testcases/elichika_tests/syntax/UserDefinedFunc.py
@@ -15,6 +15,25 @@ def h(x, y):
     return x + y
 
 
+class G(object):
+    def __init__(self, a, do):
+        self.a = a
+        self.do = do
+
+    def func1(self):
+        temp = None
+        if self.do:
+            temp = self.a
+        else:
+            temp = self
+        return temp
+    
+    def func2(self):
+        return self
+
+    def func3(self):
+        return self.a
+ 
 class A(chainer.Chain):
 
     def __init__(self):
@@ -42,6 +61,16 @@ class C(chainer.Chain):
         p = UserDefinedFuncSub1.F(x).g(y)
         return h(p, z)
 
+class D(chainer.Chain):
+    def forward(self):
+        ret = 0
+        obj = G(ret, False)
+        ret += obj.func1().a            # TODO(rchouras): Commenting this line passes the test.
+        ret += obj.func2().a
+        ret += obj.func3()
+        return ret
+
+
 # ======================================
 
 from chainer_compiler.elichika import testtools
@@ -57,6 +86,7 @@ def main():
     testtools.generate_testcase(model, [a, b, c])
     testtools.generate_testcase(B(), [a, b, c], subname='external_func')
     testtools.generate_testcase(C(), [a, b, c], subname='external_class')
+    # testtools.generate_testcase(D(), [], subname='bug')
 
 if __name__ == '__main__':
     main()
