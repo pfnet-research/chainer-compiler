b500189394fb65636a2a9f0e6c1f4352abb8ec1e
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-07-02 03:45:32 +0530

    test: Added test `self_not_returned`.

--- testcases/elichika_tests/syntax/With.py
+++ testcases/elichika_tests/syntax/With.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 
 import chainer
-from chainer_compiler.elichika.parser.flags import ignore_branch
+from chainer_compiler.elichika.parser import flags
 
-class F(object):
+class X(object):
     def __init__(self, a):
         self.a = a
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        return False
+
+class Y(X):
     def __enter__(self):
         self.a = self.a + 1
         return self
@@ -15,30 +22,43 @@ class F(object):
         self.a = self.a + 2
         return False
 
+class Z(X):
+    def __enter__(self):
+        return self.a
+
+# ======================================
+
 class A(chainer.Chain):
     def forward(self):
-        with F(3) as obj:
+        with X(3) as obj:
             return obj.a
 
 class B(chainer.Chain):
     def forward(self):
         ret = 0
-        with F(3) as obj1, F(4) as obj2:
-            ret = obj1.a + obj2.a
+        with X(3) as obj1, X(4) as obj2:
+            ret += obj1.a
+        ret += obj2.a
         return ret
 
 class C(chainer.Chain):
     def forward(self):
         ret = 0
-        with F(3) as obj1, F(4) as obj2:
+        obj2 = Y(4)
+        with Y(3) as obj1:
             ret += obj1.a
         ret +=  obj2.a
         return ret
 
+class D(chainer.Chain):
+    def forward(self):
+        with Z(3) as obj:
+            return obj
+
 class IgnoreBranch(chainer.Chain):
     def forward(self):
         ret = 0
-        with ignore_branch():
+        with flags.ignore_branch():
             ret += 1
         ret += 1
         return ret
@@ -54,6 +74,7 @@ def main():
     testtools.generate_testcase(A(), [], 'basic')
     testtools.generate_testcase(B(), [], 'multiple')
     testtools.generate_testcase(C(), [], 'enter_exit')
+    testtools.generate_testcase(D(), [], 'self_not_returned')
     # testtools.generate_testcase(IgnoreBranch(), [], 'ignore_branch')
 
 
