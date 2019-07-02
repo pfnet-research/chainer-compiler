558b148ce9b30dfae93d92ac67b073cad670c5ce
take-cheeze <takechi101010@gmail.com>
2019-07-02 17:56:28 +0900

    Fix quantizer script downloading

--- utils/quantize.sh
+++ utils/quantize.sh
@@ -3,9 +3,10 @@
 set -eu
 
 SCRIPT_DIR=$(dirname "$0")
+QUANTIZER_LIB="$SCRIPT_DIR/../build/quantize.py"
 
-if [ ! -f "$SCRIPT_DIR/../scripts/quantize.py" ] ; then
-    curl -L 'https://raw.githubusercontent.com/microsoft/onnxruntime/master/onnxruntime/python/tools/quantization/quantize.py' -o "$SCRIPT_DIR/../build/quantize.py"
+if [ ! -f "$QUANTIZER_LIB" ] ; then
+    curl -L 'https://raw.githubusercontent.com/microsoft/onnxruntime/master/onnxruntime/python/tools/quantization/quantize.py' -o "$QUANTIZER_LIB"
 fi
 
 export PYTHONPATH="$SCRIPT_DIR/../build"
