77cca07b8fd67828f31d169052ea06a0efaa20da
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-07-02 09:33:24 +0900

    Use dldt directory specified by env/cmake

--- compiler/chxvm/emitter.cc
+++ compiler/chxvm/emitter.cc
@@ -944,8 +944,13 @@ private:
                 CHECK(xmodel.SerializeToOstream(&ofs));
             }
 
+            const char* dldt_dir_env = getenv("CHAINER_COMPILER_DLDT_DIR");
+            std::string dldt_dir = dldt_dir_env ? dldt_dir_env : CHAINER_COMPILER_DLDT_DIR;
+            CHECK(!dldt_dir.empty()) << "CHAINER_COMPILER_DLDT_DIR is not set properly";
             const std::string cmdline =
-                    StrCat("python3 dldt/model-optimizer/mo_onnx.py"
+                    StrCat("python3 ",
+                           dldt_dir,
+                           "/model-optimizer/mo_onnx.py"
                            " --input_model ",
                            onnx_path,
                            " --model_name ",
