7e705cb03fabd9d8188b3c7d44158e9d8abd4000
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-07-22 18:56:01 +0900

    Implement downsample resize in ChxVM

--- runtime/ops/resize.cc
+++ runtime/ops/resize.cc
@@ -3,6 +3,7 @@
 #include <chainerx/array.h>
 #include <chainerx/routines/creation.h>
 #include <chainerx/routines/manipulation.h>
+#include <chainerx/routines/pooling.h>
 
 #include <common/log.h>
 #include <runtime/chainerx_util.h>
@@ -99,23 +100,60 @@ chainerx::Array Upsample2D32bitForCPU(chainerx::Array x, const chainerx::Shape& 
     return y;
 }
 
+chainerx::Array Downsample(const chainerx::Array& x, const std::vector<int64_t> scales) {
+    int64_t total_scale = 1;
+    chainerx::Shape to_shape(x.shape());
+    for (size_t i = 0; i < scales.size(); ++i) {
+        int64_t scale = scales[i];
+        CHECK_EQ(0, to_shape[i] % scale) << "Unsupported downsampling: shape=" << x.shape() << " scale=" << scale;
+        to_shape[i] /= scale;
+        total_scale *= scale;
+    }
+    CHECK_EQ(4, to_shape.size());
+    CHECK_EQ(1, scales[0]);
+    CHECK_EQ(1, scales[1]);
+    chainerx::Dims ksize{scales[2], scales[3]};
+    chainerx::Dims stride{scales[2], scales[3]};
+    chainerx::Dims pad{0, 0};
+    chainerx::Array y = chainerx::AveragePool(x, ksize, stride, pad);
+    y *= total_scale;
+    return y;
+}
+
 }  // namespace
 
 chainerx::Array UpsampleOp::RunImpl(ChxVMState* st, const chainerx::Array& x, const chainerx::Array& scales) {
     CHECK_EQ(1, scales.ndim());
+
+    bool has_upsample = false;
+    bool has_downsample = false;
+
     std::vector<int64_t> int_scales;
     for (int64_t i = 0; i < scales.shape()[0]; ++i) {
         chainerx::Scalar scale = chainerx::AsScalar(scales.At({i}));
         int64_t int_scale;
         if (scale.kind() == chainerx::DtypeKind::kFloat) {
             double double_scale = static_cast<double>(scale);
+            if (double_scale > 1.0) {
+                has_upsample = true;
+            } else if (double_scale < 1.0) {
+                double_scale = 1.0 / double_scale;
+                has_downsample = true;
+            }
             int_scale = static_cast<int64_t>(std::round(double_scale));
             CHECK_EQ(double_scale, int_scale) << "Only int scale is supported: " << scales;
         } else {
             int_scale = static_cast<int64_t>(scale);
+            has_upsample = int_scale > 1;
         }
         CHECK_LE(1, int_scale) << "scales must be greater than or equal to 1: " << scales;
         int_scales.push_back(int_scale);
+    }
+
+    CHECK(!has_upsample || !has_downsample) << "Resize with both upsampling and downsampling is not supported: scales=" << scales;
+
+    if (has_downsample) {
+        return Downsample(x, int_scales);
     }
 
     chainerx::Shape to_shape(x.shape());
