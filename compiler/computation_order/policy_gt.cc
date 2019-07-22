4caba1135116d7380d19851fc25a864f90dc913a
Mitsuru Kusumoto <mkusumoto@preferred.jp>
2019-07-22 17:35:35 +0900

    specify namespace

--- compiler/computation_order/policy_gt.cc
+++ compiler/computation_order/policy_gt.cc
@@ -354,7 +354,7 @@ std::vector<Order> ComputeOrder(const Graph& graph, const SimpleGraph& sg, const
 }
 
 int64_t AutomaticBudgetDetection() {
-    g_meminfo_enabled = true;
+    runtime::g_meminfo_enabled = true;
     auto info = runtime::GetMemoryUsageInBytes();
     if (!info)
         return 100000000000LL;
