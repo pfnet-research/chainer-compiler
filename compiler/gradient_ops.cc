be7faf1469fe6d709a0c163eca604851f04d7396
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-08-01 20:02:31 +0900

    Propagate Conv params to ConvGradWeight

--- compiler/gradient_ops.cc
+++ compiler/gradient_ops.cc
@@ -452,7 +452,12 @@ void ConvGradFn(GradientOpContext* gc) {
                     ->set_strides(node->strides());
         }
     }
-    gc->GradOp(Node::kChainerConvGradWeight, 1, {w, gc->x(0), gy})->producer()->set_strides(node->strides())->set_pads(node->pads());
+    gc->GradOp(Node::kChainerConvGradWeight, 1, {w, gc->x(0), gy})
+        ->producer()
+        ->set_dilations(node->dilations())
+        ->set_group(node->group())
+        ->set_pads(node->pads())
+        ->set_strides(node->strides());
     if (node->inputs().size() == 3) {
         std::vector<int64_t> axes{{0}};
         CHECK(!node->kernel_shape().empty()) << "ConvGrad with no kernel_shape is not supported yet.";
@@ -476,7 +481,12 @@ void ConvTransposeGradFn(GradientOpContext* gc) {
                 ->set_pads(node->pads())
                 ->set_strides(node->strides());
     }
-    gc->GradOp(Node::kChainerConvGradWeight, 1, {w, gy, gc->x(0)})->producer()->set_strides(node->strides())->set_pads(node->pads());
+    gc->GradOp(Node::kChainerConvGradWeight, 1, {w, gy, gc->x(0)})
+        ->producer()
+        ->set_dilations(node->dilations())
+        ->set_group(node->group())
+        ->set_pads(node->pads())
+        ->set_strides(node->strides());
     if (node->inputs().size() == 3) {
         std::vector<int64_t> axes{{0}};
         CHECK(!node->kernel_shape().empty()) << "ConvGrad with no kernel_shape is not supported yet.";
