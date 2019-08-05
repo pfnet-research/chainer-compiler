e7e50bcb7d570ba2bbf373e0efe92aa84079775b
momohatt <momohatt10@gmail.com>
2019-08-05 14:46:14 +0900

    Add astmonkey visualization

--- chainer_compiler/elichika/parser/typing.py
+++ chainer_compiler/elichika/parser/typing.py
@@ -3,7 +3,36 @@ import gast
 class TypeChecker(gast.NodeTransformer):
     def __init__(self):
         super().__init__()
-        pass
+        # type environment
+        self.tyenv = {}
 
     def generic_visit(self, node):
         return super().generic_visit(node)
+
+if __name__ == '__main__':
+    import ast
+    import gast
+    import sys
+    from copy import deepcopy
+    try:
+        from astmonkey import transformers, visitors
+        IMPORT_ASTMONKEY = True
+    except ImportError:
+        IMPORT_ASTMONKEY = False
+
+    def dump_ast(mod, name):
+        print(gast.dump(mod))
+        if IMPORT_ASTMONKEY:
+            mod = deepcopy(mod)
+            mod = transformers.ParentChildNodeTransformer().visit(deepcopy(mod))
+            visitor = visitors.GraphNodeVisitor()
+            visitor.visit(mod)
+            visitor.graph.write_png(name + '.png')
+            print("\033[1;32;40mAST visualization saved as \033[94m%s.png\033[0m" % name)
+        else:
+            print("\033[93mInstall astmonkey for visualization.\033[0m")
+
+    code = open(sys.argv[1]).read()
+    orig_ast = gast.ast_to_gast(ast.parse(code))
+    print('=== Original AST ===')
+    dump_ast(orig_ast, 'original')
