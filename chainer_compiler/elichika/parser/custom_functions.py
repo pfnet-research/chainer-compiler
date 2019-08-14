905d2ea920399f79ab45a02aaf2cd626fd59f724
durswd <swda.durl@gmail.com>
2019-08-14 16:52:36 +0900

    implemented stacktrace

--- chainer_compiler/elichika/parser/custom_functions.py
+++ chainer_compiler/elichika/parser/custom_functions.py
@@ -1,16 +1,16 @@
 import chainer.functions as F
 import numpy as np
 
-def check_attribute_value(actual_value, expected_value, func_name=''):
+def check_attribute_value(actual_value, expected_value, func_name='', arg_name=''):
     return
 
-def check_attribute_scalar(value, func_name=''):
+def check_attribute_scalar(value, func_name='', arg_name=''):
     return
 
 def numpy_clip(a, a_min, a_max, out=None):
-    check_attribute_scalar(a_min,'numpy.clip')
-    check_attribute_scalar(a_max,'numpy.clip')
-    check_attribute_value(out, None, 'numpy.clip')
+    check_attribute_scalar(a_min,'numpy.clip', 'a_min')
+    check_attribute_scalar(a_max,'numpy.clip', 'a_max')
+    check_attribute_value(out, None, 'numpy.clip', 'out')
 
     a = F.clip(a, a_min, a_max)
     return a
