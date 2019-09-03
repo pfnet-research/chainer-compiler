69b2cae00b8e51238066f388405cfb20348c2999
momohatt <momohatt10@gmail.com>
2019-09-03 18:41:58 +0900

    Fixing issue of too long debug dumps

--- chainer_compiler/elichika/parser/type_checker.py
+++ chainer_compiler/elichika/parser/type_checker.py
@@ -16,9 +16,84 @@ import chainer.functions as F
 import chainer.links as L
 import numpy as np
 
-def debug(sth):
-    frame = inspect.currentframe().f_back
-    print("[{} {}] {}".format(frame.f_code.co_name, frame.f_lineno, sth))
+# ============================ Display utils ===================================
+
+def intercalate(strings, sep):
+    return "".join([s + sep for s in strings[:-1]]) + strings[-1]
+
+
+def expr_to_str(node):
+    if isinstance(node, gast.BoolOp):
+        return intercalate([expr_to_str(e) for e in node.values],
+                " " + boolop_to_str(node.op) + " ")
+    if isinstance(node, gast.BinOp):
+        return "{} {} {}".format(expr_to_str(node.left),
+                operator_to_str(node.op), expr_to_str(node.right))
+    if isinstance(node, gast.Call):
+        return "{}({})".format(expr_to_str(node.func),
+                intercalate([expr_to_str(arg) for arg in node.args], ", "))
+    if isinstance(node, gast.Num):
+        return str(node.n)
+    if isinstance(node, gast.Str):
+        return str(node.s)
+    if isinstance(node, gast.Attribute):
+        return "{}.{}".format(expr_to_str(node.value), node.attr)
+    if isinstance(node, gast.Name):
+        return node.id
+    return ""
+
+
+def boolop_to_str(node):
+    if isinstance(node, gast.And):
+        return "and"
+    if isinstance(node, gast.Or):
+        return "or"
+
+
+def operator_to_str(node):
+    if isinstance(node, gast.Add):
+        return "+"
+    if isinstance(node, gast.Sub):
+        return "-"
+    if isinstance(node, gast.Mult):
+        return "*"
+    if isinstance(node, gast.Div):
+        return "/"
+    if isinstance(node, gast.FloorDiv):
+        return "//"
+
+
+def is_expr(node):
+    return isinstance(node, gast.BoolOp) \
+            or isinstance(node, gast.BinOp) \
+            or isinstance(node, gast.UnaryOp) \
+            or isinstance(node, gast.Lambda) \
+            or isinstance(node, gast.IfExp) \
+            or isinstance(node, gast.Dict) \
+            or isinstance(node, gast.Set) \
+            or isinstance(node, gast.ListComp) \
+            or isinstance(node, gast.SetComp) \
+            or isinstance(node, gast.DictComp) \
+            or isinstance(node, gast.GeneratorExp) \
+            or isinstance(node, gast.Await) \
+            or isinstance(node, gast.Yield) \
+            or isinstance(node, gast.YieldFrom) \
+            or isinstance(node, gast.Compare) \
+            or isinstance(node, gast.Call) \
+            or isinstance(node, gast.Repr) \
+            or isinstance(node, gast.Num) \
+            or isinstance(node, gast.Str) \
+            or isinstance(node, gast.FormattedValue) \
+            or isinstance(node, gast.JoinedStr) \
+            or isinstance(node, gast.Bytes) \
+            or isinstance(node, gast.NameConstant) \
+            or isinstance(node, gast.Ellipsis) \
+            or isinstance(node, gast.Attribute) \
+            or isinstance(node, gast.Subscript) \
+            or isinstance(node, gast.Starred) \
+            or isinstance(node, gast.Name) \
+            or isinstance(node, gast.List) \
+            or isinstance(node, gast.Tuple)
 
 
 def node_description(node):
@@ -26,11 +101,15 @@ def node_description(node):
     lineno = " (line {})".format(node.lineno) if hasattr(node, 'lineno') else ""
     if isinstance(node, gast.FunctionDef):
         return "{} {}{}".format(type_name, node.name, lineno)
-    if isinstance(node, gast.Name):
-        return "{} {}{}".format(type_name, node.id, lineno)
-    if isinstance(node, gast.Attribute):
-        return "{} {}{}".format(type_name, node.attr, lineno)
+    if is_expr(node):
+        return "{} {}{}".format(type_name, expr_to_str(node), lineno)
     return type_name
+
+# ==============================================================================
+
+def debug(sth):
+    frame = inspect.currentframe().f_back
+    print("[{} {}] {}".format(frame.f_code.co_name, frame.f_lineno, sth))
 
 
 def defined_with___call__(func):
@@ -178,7 +257,8 @@ def ty_ChainerConcat(ty_args, dummy_args_nontensor, kwargs):
 
 def ty_ChainerExpandDims(ty_args, dummy_args_nontensor, kwargs):
     # TODO(momohatt): axis can come as dummy_args_nontensor
-    axis = kwargs['axis']
+    axis = kwargs['axis'] if dummy_args_nontensor == [] else \
+            dummy_args_nontensor[0]
     fallback_shapes = ((1,) * axis,)
     fallback_dtypes = (np.float32,)
 
@@ -191,7 +271,8 @@ def ty_ChainerBroadcastTo(ty_args, dummy_args_nontensor, kwargs):
 
 
 def ty_ChainerSum(ty_args, dummy_args_nontensor, kwargs):
-    axis = kwargs['axis']
+    axis = kwargs['axis'] if dummy_args_nontensor == [] else \
+            dummy_args_nontensor[0]
     fallback_shapes = ((1,) * (axis + 1),)
     fallback_dtypes = (np.float32,)
 
