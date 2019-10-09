ab274c37ec0da1826d8977a2ee5b35289460fd49
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-10-09 13:43:22 +0900

    Add --opset_version flag to adjust shape inference

--- compiler/onnx.cc
+++ compiler/onnx.cc
@@ -1,10 +1,13 @@
 #include <compiler/onnx.h>
 
+#include <compiler/flags.h>
+
 namespace chainer_compiler {
 
 std::unordered_map<std::string, int> OpsetImports() {
+    const int64_t opset_version = g_opset_version ? g_opset_version : DEFAULT_OPSET_VERSION;
     return {
-            {onnx::ONNX_DOMAIN, DEFAULT_OPSET_VERSION},
+            {onnx::ONNX_DOMAIN, opset_version},
             {CHAINER_ONNX_DOMAIN, 9},
     };
 }
--- scripts/generate_flags_code.py
+++ scripts/generate_flags_code.py
@@ -98,6 +98,10 @@ FLAGS = {
         'type': 'bool',
         'doc': 'Use fixed batch normalization even for backprop.'
     },
+    'opset_version': {
+        'type': 'int',
+        'doc': 'Set the opset_version for shape inference.'
+    },
 
     'trace_level': {
         'type': 'int',
